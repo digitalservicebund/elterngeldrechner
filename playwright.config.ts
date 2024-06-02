e598f26cb542129fa6508dc23fd3fc0e14a65a63
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2024-06-02 19:03:32 +0200

    Test: update styles testing to include more viewports

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,20 +40,20 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    // },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
 
-    {
-      name: "iphone",
-      use: { ...devices["iPhone 13"] },
-    },
+    // {
+    //   name: "iphone",
+    //   use: { ...devices["iPhone 13"] },
+    // },
 
     /* Test against mobile viewports. */
     // {

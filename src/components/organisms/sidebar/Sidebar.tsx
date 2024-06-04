7ac7b84426ea5c038e3f96384460229323a71144
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2024-06-04 15:12:40 +0200

    Refactor(Sidebar): get step label from fromSteps object

--- src/components/organisms/sidebar/Sidebar.tsx
+++ src/components/organisms/sidebar/Sidebar.tsx
@@ -18,28 +18,13 @@ export function Sidebar({ currentStep }: Props) {
     (step) => step === currentStep,
   );
 
-  const stepNumber = () => {
-    switch (currentStep.route) {
-      case "/nachwuchs":
-        return "2/7";
-      case "/erwerbstaetigkeit":
-        return "3/7";
-      case "/einkommen":
-        return "4/7";
-      case "/elterngeldvarianten":
-        return "5/7";
-      case "/rechner-planer":
-        return "6/7";
-      case "/zusammenfassung-und-daten":
-        return "7/7";
-      default:
-        return "1/7";
-    }
-  };
+  const stepsTotal = Object.entries(formSteps).length;
+
+  const stepLabel = `${currentStepIndex + 1}/${stepsTotal}`;
 
   const buttonLabel = (
     <>
-      <strong>{stepNumber()}</strong>
+      <strong>{stepLabel}</strong>
       {currentStep.text}
     </>
   );

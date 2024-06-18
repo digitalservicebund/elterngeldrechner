71ebec63aa12c04ee5fd717c1c70b0eb6d67b287
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2024-06-18 13:54:51 +0200

    Build: disable minification in production build

--- craco.config.js
+++ craco.config.js
@@ -7,6 +7,12 @@ module.exports = {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+    configure: (webpackConfig, { env, paths }) => {
+      // disable minification in production build
+      // files will be minified by the CMS' minifier
+      webpackConfig.optimization.minimize = false;
+      return webpackConfig;
+    },
   },
   jest: {
     configure: {

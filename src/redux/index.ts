6d9eef5f808829d6b4f050e27978f48348b83788
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2024-06-27 12:22:11 +0200

    Reapply "Build: disconnect preloadedState from dev environment"
    This reverts commit 5a7f25b692463aea7a9ee1b5d9d347d001a56d30.
    
    After all wasn't such a bad idea. E.g. I want to disable preloadedState
    when using playwright codegen which accesses the running development
    app.

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -21,7 +21,7 @@ export const reducers = combineReducers({
 const store = configureStore({
   reducer: reducers,
   preloadedState:
-    process.env.NODE_ENV === "development" ? preloadedState : undefined,
+    process.env.VITE_APP_PRELOAD_STATE === "true" ? preloadedState : undefined,
 });
 
 export default store;

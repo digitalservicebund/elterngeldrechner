bbfe8450f4bdf29f51d8764f7f74f9fca5d6f37a
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2024-06-27 11:56:36 +0200

    Build: disconnect preloadedState from dev environment
    I might want to have NODE_ENV=development but without preloadedState,
    e.g. in e2e tests. Somehow it's not so easy to set NODE_ENV=test.

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -21,7 +21,7 @@ export const reducers = combineReducers({
 const store = configureStore({
   reducer: reducers,
   preloadedState:
-    process.env.NODE_ENV === "development"
+    process.env.REACT_APP_PRELOAD_STATE === "true"
       ? (preloadedState as any)
       : undefined,
 });

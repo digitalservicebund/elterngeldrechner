f923ef23a2f58b1a9fd8ea3f1cb28b9c954dd7b4
Thore Strassburg <thore@strassburg.me>
2024-07-01 17:07:09 +0200

    Chore: configure base path for deployment
    The former build setup with create-react-app used the `homepage` option
    in the `package.json`. This was important to make the deployment for the
    different environments working. The problem was not recognized during
    the conversation but happened during the first staging deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: ".",
   plugins: [react()],
   build: {
     // Content Management System of production environment does apply

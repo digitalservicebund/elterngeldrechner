5aff5b9ffca0b326a3a87c2964b357ce62f889e2
weilbith <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-01 14:58:51 +0000

    Automated deployment to GitHub pages (main | main | 3bc3965a0974d29fa878e07be4f09c3aed71924e)

---
+++ main/assets/Split-B7zlNWZL.js
@@ -0,0 +1,20 @@
+import { j as jsxRuntimeExports, c as classNames, n as nsp } from "./index-BSa1_k3q.js";
+function SplitItem({ hasDivider, children }) {
+  return /* @__PURE__ */ jsxRuntimeExports.jsx(
+    "div",
+    {
+      className: classNames(
+        nsp("split-item"),
+        hasDivider && nsp("split-item--has-divider")
+      ),
+      children
+    }
+  );
+}
+function Split({ children }) {
+  return /* @__PURE__ */ jsxRuntimeExports.jsx("div", { className: nsp("split"), children });
+}
+export {
+  Split as S,
+  SplitItem as a
+};

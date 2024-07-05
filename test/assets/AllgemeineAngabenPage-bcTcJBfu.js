6500bd378e96254f8863821f2ad485017cc9d452
joschka <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-05 08:34:27 +0000

    Automated deployment to GitHub pages (/test | main | 465d44b81dcd2937dc14d82fdf5a9b942cce0146)

---
+++ test/assets/AllgemeineAngabenPage-bcTcJBfu.js
@@ -0,0 +1,201 @@
+import { j as jsxRuntimeExports, c as classNames, n as nsp, Y as YesNo, r as reactExports, u as useAppDispatch, a as useNavigate, b as useAppSelector, P as Page, f as formSteps, s as stepRechnerActions, d as stepAllgemeineAngabenActions } from "./index-BwOtfhoS.js";
+import { g as get, D as Description, I as InfoDialog, u as useForm, F as FormFieldGroup, C as CustomRadio, i as infoTexts, Y as YesNoRadio } from "./Monatsplaner-DiXYiish.js";
+import { S as Split, a as SplitItem } from "./Split-CnZnhaVk.js";
+import "./egr-configuration-CMPTsO-Y.js";
+import { B as ButtonGroup } from "./ButtonGroup-Co46n01J.js";
+function CustomInput({
+  register,
+  name,
+  label,
+  errors,
+  placeholder,
+  info
+}) {
+  const error = get(errors, name);
+  return /* @__PURE__ */ jsxRuntimeExports.jsxs(
+    "div",
+    {
+      className: classNames(
+        nsp("custom-input"),
+        error && nsp("custom-input--error")
+      ),
+      children: [
+        /* @__PURE__ */ jsxRuntimeExports.jsxs("div", { className: nsp("custom-input-question"), children: [
+          /* @__PURE__ */ jsxRuntimeExports.jsx("label", { className: nsp("custom-input-question__label"), htmlFor: name, children: label }),
+          /* @__PURE__ */ jsxRuntimeExports.jsx(
+            "input",
+            {
+              className: nsp("custom-input-question__input"),
+              ...register(name),
+              type: "text",
+              id: name,
+              placeholder,
+              "aria-describedby": error ? `${name}-error` : void 0,
+              "aria-invalid": !!error
+            }
+          ),
+          !!error && /* @__PURE__ */ jsxRuntimeExports.jsx(Description, { id: `${name}-error`, error: true, children: error.message })
+        ] }),
+        !!info && /* @__PURE__ */ jsxRuntimeExports.jsx(InfoDialog, { info })
+      ]
+    }
+  );
+}
+const antragstellendeLabels = {
+  FuerBeide: "Für beide",
+  EinenElternteil: "Für einen Elternteil"
+};
+const antragstellendeOptions = [
+  { value: "FuerBeide", label: antragstellendeLabels.FuerBeide },
+  { value: "EinenElternteil", label: antragstellendeLabels.EinenElternteil }
+];
+function AllgemeineAngabenForm({
+  initialValues,
+  onSubmit,
+  handleDirtyForm
+}) {
+  const { register, handleSubmit, watch, formState } = useForm({
+    defaultValues: initialValues
+  });
+  const { isDirty, dirtyFields } = formState;
+  const antragstellendeFormValue = watch("antragstellende");
+  const mutterschaftssleistungenFormValue = watch("mutterschaftssleistungen");
+  const mutteschaftsleistungenOptions = [
+    { value: "ET1", label: watch("pseudonym.ET1") || "Elternteil 1" },
+    { value: "ET2", label: watch("pseudonym.ET2") || "Elternteil 2" }
+  ];
+  const showMutterschaftsleistungsWerGroup = antragstellendeFormValue === "FuerBeide" && mutterschaftssleistungenFormValue === YesNo.YES;
+  reactExports.useEffect(() => {
+    handleDirtyForm(isDirty, dirtyFields);
+  }, [isDirty, dirtyFields, handleDirtyForm]);
+  return /* @__PURE__ */ jsxRuntimeExports.jsx(jsxRuntimeExports.Fragment, { children: /* @__PURE__ */ jsxRuntimeExports.jsxs("form", { onSubmit: handleSubmit(onSubmit), noValidate: true, children: [
+    /* @__PURE__ */ jsxRuntimeExports.jsx("h3", { className: "mb-10", children: "Allgemeine Angaben" }),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(
+      FormFieldGroup,
+      {
+        headline: "Eltern",
+        description: "Für wen planen Sie Elterngeld?",
+        children: /* @__PURE__ */ jsxRuntimeExports.jsx(
+          CustomRadio,
+          {
+            register,
+            registerOptions: { required: "Dieses Feld ist erforderlich" },
+            name: "antragstellende",
+            errors: formState.errors,
+            options: antragstellendeOptions,
+            required: true
+          }
+        )
+      }
+    ),
+    antragstellendeFormValue === "FuerBeide" ? /* @__PURE__ */ jsxRuntimeExports.jsx(
+      FormFieldGroup,
+      {
+        headline: "Ihre Namen (optional)",
+        description: "Um auf die Begriffe Elternteil 1 und Elternteil 2 in den folgenden Schritten verzichten zu können, können Sie hier Ihre Namen oder ein Pseudonym angeben, welches wir dann verwenden werden.",
+        children: /* @__PURE__ */ jsxRuntimeExports.jsxs(Split, { children: [
+          /* @__PURE__ */ jsxRuntimeExports.jsx(SplitItem, { children: /* @__PURE__ */ jsxRuntimeExports.jsx(
+            CustomInput,
+            {
+              register,
+              name: "pseudonym.ET1",
+              label: "Name für Elternteil 1"
+            }
+          ) }),
+          /* @__PURE__ */ jsxRuntimeExports.jsx(SplitItem, { children: /* @__PURE__ */ jsxRuntimeExports.jsx(
+            CustomInput,
+            {
+              register,
+              name: "pseudonym.ET2",
+              label: "Name für Elternteil 2"
+            }
+          ) })
+        ] })
+      }
+    ) : null,
+    antragstellendeFormValue === "EinenElternteil" ? /* @__PURE__ */ jsxRuntimeExports.jsx(
+      FormFieldGroup,
+      {
+        headline: "Alleinerziehendenstatus",
+        description: "Sind Sie alleinerziehend?",
+        info: infoTexts.alleinerziehend,
+        children: /* @__PURE__ */ jsxRuntimeExports.jsx(
+          YesNoRadio,
+          {
+            register,
+            registerOptions: { required: "Dieses Feld ist erforderlich" },
+            name: "alleinerziehend",
+            errors: formState.errors,
+            required: true
+          }
+        )
+      }
+    ) : null,
+    /* @__PURE__ */ jsxRuntimeExports.jsx(jsxRuntimeExports.Fragment, { children: antragstellendeFormValue !== null ? /* @__PURE__ */ jsxRuntimeExports.jsxs(jsxRuntimeExports.Fragment, { children: [
+      /* @__PURE__ */ jsxRuntimeExports.jsx(
+        FormFieldGroup,
+        {
+          headline: "Mutterschaftsleistungen",
+          description: "Beziehen Sie Mutterschaftsleistungen?",
+          info: infoTexts.mutterschaftsleistungen,
+          children: /* @__PURE__ */ jsxRuntimeExports.jsx(
+            YesNoRadio,
+            {
+              register,
+              registerOptions: { required: "Dieses Feld ist erforderlich" },
+              name: "mutterschaftssleistungen",
+              errors: formState.errors,
+              required: true
+            }
+          )
+        }
+      ),
+      !!showMutterschaftsleistungsWerGroup && /* @__PURE__ */ jsxRuntimeExports.jsx(FormFieldGroup, { description: "Welcher Elternteil bezieht Mutterschaftsleistungen?", children: /* @__PURE__ */ jsxRuntimeExports.jsx(
+        CustomRadio,
+        {
+          register,
+          registerOptions: {
+            required: "Dieses Feld ist erforderlich"
+          },
+          name: "mutterschaftssleistungenWer",
+          options: mutteschaftsleistungenOptions,
+          errors: formState.errors,
+          required: true
+        }
+      ) })
+    ] }) : null }),
+    /* @__PURE__ */ jsxRuntimeExports.jsx(ButtonGroup, { isStepOne: true })
+  ] }) });
+}
+function AllgemeineAngabenPage() {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const initialValues = useAppSelector((state) => state.stepAllgemeineAngaben);
+  const [isFormDirty, setIsFormDirty] = reactExports.useState(false);
+  const handleSubmit = (values) => {
+    if (isFormDirty) {
+      dispatch(
+        stepRechnerActions.setHasBEGResultChangedDueToPrevFormSteps({
+          ET1: true,
+          ET2: true
+        })
+      );
+    }
+    dispatch(stepAllgemeineAngabenActions.submitStep(values));
+    navigate(formSteps.nachwuchs.route);
+  };
+  const handleDirtyForm = (isFormDirty2) => {
+    setIsFormDirty(isFormDirty2);
+  };
+  return /* @__PURE__ */ jsxRuntimeExports.jsx(Page, { step: formSteps.allgemeinAngaben, children: /* @__PURE__ */ jsxRuntimeExports.jsx(
+    AllgemeineAngabenForm,
+    {
+      initialValues,
+      onSubmit: handleSubmit,
+      handleDirtyForm
+    }
+  ) });
+}
+export {
+  AllgemeineAngabenPage as default
+};
---
+++ test/assets/egr-configuration-CMPTsO-Y.js
@@ -0,0 +1,26 @@
+import { H as commonjsGlobal, j as jsxRuntimeExports } from "./index-BwOtfhoS.js";
+var Add = {};
+var __assign = commonjsGlobal && commonjsGlobal.__assign || function() {
+  __assign = Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+      s = arguments[i];
+      for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+        t[p] = s[p];
+    }
+    return t;
+  };
+  return __assign.apply(this, arguments);
+};
+Object.defineProperty(Add, "__esModule", { value: true });
+var jsx_runtime_1 = jsxRuntimeExports;
+var SvgAdd = function(props) {
+  return (0, jsx_runtime_1.jsxs)("svg", __assign({ xmlns: "http://www.w3.org/2000/svg", height: 24, viewBox: "0 0 24 24", width: 24, "data-testid": "AddIcon", role: "graphics-symbol img", focusable: "false", "aria-hidden": "true" }, props, { children: [(0, jsx_runtime_1.jsx)("path", { d: "M0 0h24v24H0z", fill: "none" }), (0, jsx_runtime_1.jsx)("path", { d: "M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" })] }));
+};
+var _default = Add.default = SvgAdd;
+const EgrConst = {
+  lebensMonateMaxNumber: 32
+};
+export {
+  EgrConst as E,
+  _default as _
+};

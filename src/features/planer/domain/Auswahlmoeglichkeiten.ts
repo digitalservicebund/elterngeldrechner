a00aa8e879f5809806aa64750a009d5efff5d323
Thore Strassburg <thore@strassburg.me>
2024-08-13 11:49:51 +0200

    Feat(planer): add domain operation to determine Auswahlmöglichkeiten
    This closes the circle of operations to create a Plan by providing
    information about possible Auswahlmöglichkeiten for a specific
    Lebensmonat and Elternteil. It uses the Plan root entity for the
    invariants again.
    
    It is the goal of the planning feature to guide the user as much as
    possible and prevent him from creating wrong Pläne. To train the
    understanding, rules should be explained for each situation.
    Thereby, it is the strategy to provide the user with a list of possible
    Auswahlmöglichkeiten for each point he has to take a decision. If
    an Auswahlmöglichkeit is "available" but its selection would lead to an
    invalid Plan, it gets annotated as disabled with some information why.
    To achieve this, each Option is actually selected "virtually" and the
    returned result is interpreted.

---
+++ src/features/planer/domain/Auswahlmoeglichkeiten.ts
@@ -0,0 +1,31 @@
+import type { KeinElterngeld } from "./Auswahloption";
+import type { Elterngeldbezug } from "./Elterngeldbezug";
+import type { Variante } from "./Variante";
+
+export type Auswahlmoeglichkeiten = Readonly<
+  {
+    [V in Variante]: Auswahlmoeglichkeit<number>;
+  } & { [KeinElterngeld]: Auswahlmoeglichkeit<null> }
+>;
+
+export type Auswahlmoeglichkeit<E extends Elterngeldbezug = Elterngeldbezug> =
+  | EnabledAuswahlmoeglichkeit<E>
+  | DisabledAuswahlmoeglichkeit<E>;
+
+interface EnabledAuswahlmoeglichkeit<E extends Elterngeldbezug>
+  extends BasisAuswahlmoeglichkeit<E> {
+  isDisabled: false;
+  hintWhyDisabled?: undefined;
+}
+
+interface DisabledAuswahlmoeglichkeit<E extends Elterngeldbezug>
+  extends BasisAuswahlmoeglichkeit<E> {
+  isDisabled: true;
+  hintWhyDisabled: string;
+}
+
+interface BasisAuswahlmoeglichkeit<E extends Elterngeldbezug> {
+  readonly elterngeldbezug: E;
+  readonly isDisabled: boolean;
+  readonly hintWhyDisabled?: string;
+}
---
+++ src/features/planer/domain/plan/operation/bestimmeAuswahlmoeglichkeiten.ts
@@ -0,0 +1,152 @@
+import { waehleOption } from "./waehleOption";
+import type { SpecificationViolation } from "@/features/planer/domain/common/specification/SpecificationViolation";
+import type { Elterngeldbezug } from "@/features/planer/domain/Elterngeldbezug";
+import {
+  Auswahloptionen,
+  KeinElterngeld,
+  type Auswahloption,
+} from "@/features/planer/domain/Auswahloption";
+import type {
+  Ausgangslage,
+  ElternteileByAusgangslage,
+} from "@/features/planer/domain/Ausgangslage";
+import type {
+  Auswahlmoeglichkeit,
+  Auswahlmoeglichkeiten,
+} from "@/features/planer/domain/Auswahlmoeglichkeiten";
+import type { Lebensmonatszahl } from "@/features/planer/domain/Lebensmonatszahl";
+import type { Plan } from "@/features/planer/domain/plan/Plan";
+
+export function bestimmeAuswahlmoeglichkeiten<A extends Ausgangslage>(
+  plan: Plan<A>,
+  lebensmonatszahl: Lebensmonatszahl,
+  elternteil: ElternteileByAusgangslage<A>,
+): Auswahlmoeglichkeiten {
+  const elterngeldbezuege =
+    plan.errechneteElterngeldbezuege[lebensmonatszahl][elternteil];
+
+  return Object.fromEntries(
+    Auswahloptionen.map((option) => {
+      const elterngeldbezug =
+        option === KeinElterngeld ? null : elterngeldbezuege[option];
+
+      const auswahlmoeglichkeit = createAuswahlmoeglichkeit(
+        plan,
+        lebensmonatszahl,
+        elternteil,
+        option,
+        elterngeldbezug,
+      );
+
+      return [option, auswahlmoeglichkeit];
+    }),
+  ) as Auswahlmoeglichkeiten;
+}
+
+function createAuswahlmoeglichkeit<A extends Ausgangslage>(
+  plan: Plan<A>,
+  lebensmonatszahl: Lebensmonatszahl,
+  elternteil: ElternteileByAusgangslage<A>,
+  option: Auswahloption,
+  elterngeldbezug: Elterngeldbezug,
+): Auswahlmoeglichkeit {
+  return waehleOption(plan, lebensmonatszahl, elternteil, option).mapOrElse(
+    () => ({ isDisabled: false as const, elterngeldbezug }),
+    (violations) => ({
+      isDisabled: true as const,
+      hintWhyDisabled: formatViolationsAsHint(violations),
+      elterngeldbezug,
+    }),
+  );
+}
+
+function formatViolationsAsHint(violations: SpecificationViolation[]): string {
+  return violations.map((violation) => violation.message).join("\n\n");
+}
+
+if (import.meta.vitest) {
+  const { describe, it, expect } = import.meta.vitest;
+
+  describe("bestimmte Auswahlmöglichkeiten", async () => {
+    const { Elternteil } = await import("@/features/planer/domain/Elternteil");
+    const { Variante } = await import("@/features/planer/domain/Variante");
+    const { Specification, Top } = await import(
+      "@/features/planer/domain/common/specification"
+    );
+
+    it("picks up the correct Elterngeldbezug for each Auswahlmöglichkeit", () => {
+      const errechneteElterngeldbezuege = {
+        1: {
+          [Elternteil.Eins]: bezuege(111, 112, 113),
+          [Elternteil.Zwei]: bezuege(121, 122, 123),
+        },
+        2: {
+          [Elternteil.Eins]: bezuege(211, 212, 213),
+          [Elternteil.Zwei]: bezuege(221, 222, 223),
+        },
+      } as any;
+
+      const plan = { ...ANY_PLAN, errechneteElterngeldbezuege };
+
+      const auswahlmoeglichkeiten = bestimmeAuswahlmoeglichkeiten(
+        plan,
+        2,
+        Elternteil.Zwei,
+      );
+
+      expect(auswahlmoeglichkeiten[Variante.Basis].elterngeldbezug).toBe(221);
+      expect(auswahlmoeglichkeiten[Variante.Plus].elterngeldbezug).toBe(222);
+      expect(auswahlmoeglichkeiten[Variante.Bonus].elterngeldbezug).toBe(223);
+      expect(auswahlmoeglichkeiten[KeinElterngeld].elterngeldbezug).toBeNull();
+    });
+
+    it("creates a disabled Auswahlmöglichkeit including the violation as hint if the resulting Plan is ungültig for an Option", () => {
+      const ungueltigIfBasis = Specification.fromPredicate<Plan<Ausgangslage>>(
+        "Basiselterngeld kann nicht mehr ausgewählt werden.",
+        (plan) =>
+          plan.lebensmonate[2]?.[Elternteil.Zwei].gewaehlteOption !==
+          Variante.Basis,
+      );
+
+      const plan = {
+        ...ANY_PLAN,
+        gueltigerPlan: ungueltigIfBasis,
+        lebensmonate: {},
+      };
+
+      const auswahlmoeglichkeiten = bestimmeAuswahlmoeglichkeiten(
+        plan,
+        2,
+        Elternteil.Zwei,
+      );
+
+      expect(auswahlmoeglichkeiten[Variante.Basis].isDisabled).toBe(true);
+      expect(auswahlmoeglichkeiten[Variante.Basis].hintWhyDisabled).toBe(
+        "Basiselterngeld kann nicht mehr ausgewählt werden.",
+      );
+      expect(auswahlmoeglichkeiten[Variante.Plus].isDisabled).toBe(false);
+      expect(auswahlmoeglichkeiten[Variante.Bonus].isDisabled).toBe(false);
+      expect(auswahlmoeglichkeiten[KeinElterngeld].isDisabled).toBe(false);
+    });
+
+    const bezuege = function (basis: number, plus: number, bonus: number) {
+      return {
+        [Variante.Basis]: basis,
+        [Variante.Plus]: plus,
+        [Variante.Bonus]: bonus,
+      };
+    };
+
+    const ANY_PLAN = {
+      ausgangslage: { anzahlElternteile: 2 as const },
+      errechneteElterngeldbezuege: {
+        2: {
+          [Elternteil.Eins]: bezuege(0, 0, 0),
+          [Elternteil.Zwei]: bezuege(0, 0, 0),
+        },
+      } as any,
+      lebensmonate: {},
+      gueltigerPlan: Top,
+    };
+  });
+}

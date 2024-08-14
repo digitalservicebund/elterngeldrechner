c953ea319f855b40c245b5618f0b0eba3c996475
Thore Strassburg <thore@strassburg.me>
2024-08-14 13:45:47 +0200

    Feat(planer): add specification to check Kontingent
    Notice that this is the first specification actually implemented as
    a class. That is due to its dynamic violation messages. Also would
    a break down into smaller specifications also for lower data structures
    actually not help, but rather make it more complicated.
    
    The function on top of the class replaces the need to call the
    constructor itself. That's for more uniform code. In best case, most
    specifications are just constants. Especially if they are not
    configurable. Those which are, should generate specifications by
    function parameters. The classes themselves are only for the base
    implementation that provides the composability plus base methods for
    interactions. Else only specifications with special needs should need to
    implement a class.
    But because of the current technical debt of generic pass-through,
    a constant version is not possible here.

--- src/features/planer/domain/plan/specification/GueltigerPlan.ts
+++ src/features/planer/domain/plan/specification/GueltigerPlan.ts
@@ -1,9 +1,10 @@
+import { KontingentWurdeEingehalten } from "./KontingentWurdeEingehalten";
 import { MonateMitMutterschutzSindUnveraendert } from "./MonateMitMutterschutzSindUnveraendert";
 import { NurEinLebensmonatBasisParallel } from "./NurEinenLebensmonatBasisParallel";
 import type { Ausgangslage } from "@/features/planer/domain/ausgangslage";
 
 export function GueltigerPlan<A extends Ausgangslage>() {
-  return MonateMitMutterschutzSindUnveraendert<A>().and(
-    NurEinLebensmonatBasisParallel<A>(),
-  );
+  return MonateMitMutterschutzSindUnveraendert<A>()
+    .and(NurEinLebensmonatBasisParallel<A>())
+    .and(KontingentWurdeEingehalten<A>());
 }
---
+++ src/features/planer/domain/plan/specification/KontingentWurdeEingehalten.ts
@@ -0,0 +1,123 @@
+import { zaehleVerplantesKontingent } from "@/features/planer/domain/lebensmonate";
+import { SpecificationResult } from "@/features/planer/domain/common/specification/SpecificationResult";
+import {
+  Ausgangslage,
+  bestimmeVerfuegbaresKontingent,
+} from "@/features/planer/domain/ausgangslage";
+import {
+  Specification,
+  type SpecificationViolation,
+} from "@/features/planer/domain/common/specification";
+import type { Plan } from "@/features/planer/domain/plan/Plan";
+import { listeKontingentAuf } from "@/features/planer/domain/verfuegbares-kontingent";
+import type { Variante } from "@/features/planer/domain/Variante";
+
+export function KontingentWurdeEingehalten<A extends Ausgangslage>() {
+  return new KontingentWurdeEingehaltenSpecification<A>();
+}
+
+class KontingentWurdeEingehaltenSpecification<
+  A extends Ausgangslage,
+> extends Specification<Plan<A>> {
+  evaluate(plan: Plan<A>): SpecificationResult {
+    const verfuegbar = bestimmeVerfuegbaresKontingent(plan.ausgangslage);
+    const verplant = zaehleVerplantesKontingent(plan.lebensmonate);
+
+    const violations = listeKontingentAuf(verfuegbar).reduce(
+      (violations: SpecificationViolation[], [variante, maximum]) =>
+        verplant[variante] > maximum
+          ? [...violations, composeKontingentViolation(variante, maximum)]
+          : violations,
+      [],
+    );
+
+    return violations.length === 0
+      ? SpecificationResult.satisfied
+      : SpecificationResult.unsatisfied(violations[0], ...violations.splice(1));
+  }
+}
+
+function composeKontingentViolation(
+  variante: Variante,
+  maximum: number,
+): SpecificationViolation {
+  return {
+    message: `Das Kontingent von ${maximum} ${variante} Monaten ist aufgebraucht.`,
+  };
+}
+
+if (import.meta.vitest) {
+  const { describe, it, expect } = import.meta.vitest;
+
+  describe("Kontingent wurde eingehalten", async () => {
+    const { Variante } = await import("@/features/planer/domain/Variante");
+    const { KeinElterngeld } = await import(
+      "@/features/planer/domain/Auswahloption"
+    );
+
+    it("is satisfied if verplantes Kontingent remains below verfügbares Kontingent", () => {
+      vi.mocked(bestimmeVerfuegbaresKontingent).mockReturnValue(
+        kontingent(2, 4, 3),
+      );
+      vi.mocked(zaehleVerplantesKontingent).mockReturnValue(
+        kontingent(1.5, 3, 1),
+      );
+
+      expect(KontingentWurdeEingehalten().asPredicate(ANY_PLAN)).toBe(true);
+    });
+
+    it("is satisfied if verplantes Kontingent fully used verfügbares Kontingent", () => {
+      vi.mocked(bestimmeVerfuegbaresKontingent).mockReturnValue(
+        kontingent(2, 4, 3),
+      );
+      vi.mocked(zaehleVerplantesKontingent).mockReturnValue(
+        kontingent(2, 4, 3),
+      );
+
+      expect(KontingentWurdeEingehalten().asPredicate(ANY_PLAN)).toBe(true);
+    });
+
+    it("is unsatisfied if verplantes Kontingent is above verfügbares Kontingent", () => {
+      vi.mocked(bestimmeVerfuegbaresKontingent).mockReturnValue(
+        kontingent(2, 4, 3),
+      );
+      vi.mocked(zaehleVerplantesKontingent).mockReturnValue(
+        kontingent(3, 6, 4),
+      );
+
+      const violationMessages = KontingentWurdeEingehalten()
+        .evaluate(ANY_PLAN)
+        .mapOrElse(
+          () => [],
+          (violations) => violations.map(({ message }) => message),
+        );
+
+      expect(violationMessages).toHaveLength(3);
+      expect(violationMessages).toEqual(
+        expect.arrayContaining([
+          "Das Kontingent von 2 Basiselterngeld Monaten ist aufgebraucht.",
+          "Das Kontingent von 4 ElterngeldPlus Monaten ist aufgebraucht.",
+          "Das Kontingent von 3 Partnerschaftsbonus Monaten ist aufgebraucht.",
+        ]),
+      );
+    });
+
+    const kontingent = function (basis: number, plus: number, bonus: number) {
+      return {
+        [Variante.Basis]: basis,
+        [Variante.Plus]: plus,
+        [Variante.Bonus]: bonus,
+        [KeinElterngeld]: 0,
+      };
+    };
+
+    const ANY_PLAN = {} as any;
+
+    vi.mock(
+      "@/features/planer/domain/lebensmonate/operation/zaehleVerplantesKontingent",
+    );
+    vi.mock(
+      "@/features/planer/domain/ausgangslage/operation/bestimmeVerfuegbaresKontingent",
+    );
+  });
+}

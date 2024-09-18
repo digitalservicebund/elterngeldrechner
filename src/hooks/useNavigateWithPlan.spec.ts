b6a1c09d6dc5ea7a92875deed4fd15f7f35616f5
Thore Strassburg <thore@strassburg.me>
2024-09-18 15:37:03 +0200

    feat: add new hook to navigate and pass on a Plan
    This is an alternative and cheap approach to pass the planned Plan
    between the Planer page and the Zusammenfassung. It simply uses the
    browsers history state. It is a replacement for a state. There is no
    real state management necessary for this part yet. Only the Planer edits
    it and the rest just reads/views the Plan.
    That means it might get replaced with a more advanced solution once
    needed.
    
    Defining this behavior as a hook helps with the unified "encoding" and
    usage. It also makes it easily testable in a decoupled manner.
    
    Tests are split into a specification file instead of in-source tests,
    because the latter cause global mocking issues right now.

---
+++ src/hooks/useNavigateWithPlan.spec.ts
@@ -0,0 +1,81 @@
+import { renderHook } from "@testing-library/react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigateWithPlan } from "./useNavigateWithPlan";
+import { Elternteil } from "@/features/planer/domain";
+
+vi.mock("react-router-dom");
+
+describe("use navigate with Plan", () => {
+  describe("plan state", () => {
+    it("takes the plan property from the history state when defined", () => {
+      vi.mocked(useLocation).mockReturnValue({
+        ...ANY_LOCATION,
+        state: { plan: "untyped fake plan" },
+      });
+
+      const { result } = renderHook(() => useNavigateWithPlan());
+
+      expect(result.current.plan).toEqual("untyped fake plan");
+    });
+
+    it("returns and undefined Plan if the history state is null", () => {
+      vi.mocked(useLocation).mockReturnValue({
+        ...ANY_LOCATION,
+        state: null,
+      });
+
+      const { result } = renderHook(() => useNavigateWithPlan());
+
+      expect(result.current.plan).toBeUndefined();
+    });
+  });
+
+  describe("navigate with plan state", () => {
+    it("sets the given plan as history state", () => {
+      const navigate = vi.fn();
+      vi.mocked(useNavigate).mockReturnValue(navigate);
+
+      const { result } = renderHook(() => useNavigateWithPlan());
+      result.current.navigateWithPlanState("anywhere", ANY_PLAN);
+
+      expect(navigate).toHaveBeenCalledOnce();
+      expect(navigate).toHaveBeenLastCalledWith(expect.anything(), {
+        state: { plan: ANY_PLAN },
+      });
+    });
+
+    it("navigates to the given location", () => {
+      const navigate = vi.fn();
+      vi.mocked(useNavigate).mockReturnValue(navigate);
+
+      const { result } = renderHook(() => useNavigateWithPlan());
+      result.current.navigateWithPlanState("/cool-page", undefined);
+
+      expect(navigate).toHaveBeenCalledOnce();
+      expect(navigate).toHaveBeenLastCalledWith(
+        "/cool-page",
+        expect.anything(),
+      );
+    });
+  });
+});
+
+const ANY_LOCATION = {
+  pathname: "pathname",
+  state: null,
+  key: "",
+  search: "",
+  hash: "",
+};
+
+const ANY_PLAN = {
+  ausgangslage: {
+    anzahlElternteile: 1 as const,
+    pseudonymeDerElternteile: {
+      [Elternteil.Eins]: "Jane",
+    },
+    geburtsdatumDesKindes: new Date(),
+  },
+  lebensmonate: {},
+  errechneteElterngeldbezuege: {} as any,
+};
---
+++ src/hooks/useNavigateWithPlan.ts
@@ -0,0 +1,20 @@
+import { useLocation, useNavigate, type To } from "react-router-dom";
+import { useCallback } from "react";
+import type { PlanMitBeliebigenElternteilen } from "@/features/planer";
+
+export function useNavigateWithPlan() {
+  const location = useLocation();
+  const state = location?.state as StateWithOptionalPlan;
+  const plan = state ? state.plan : undefined;
+
+  const navigate = useNavigate();
+  const navigateWithPlanState = useCallback(
+    (to: To, plan: PlanMitBeliebigenElternteilen | undefined) =>
+      navigate(to, { state: { plan } }),
+    [navigate],
+  );
+
+  return { plan, navigateWithPlanState };
+}
+
+type StateWithOptionalPlan = null | { plan?: PlanMitBeliebigenElternteilen };

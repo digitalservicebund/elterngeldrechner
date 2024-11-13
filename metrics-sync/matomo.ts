8a24b9bafa1548e79750bbf80c0231889504c221
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2024-11-13 16:08:10 +0100

    feat(metabase): change env prefix from metrics to metrics_sync

--- metrics-sync/matomo.ts
+++ metrics-sync/matomo.ts
@@ -3,7 +3,7 @@ import { MatomoEventsCategoryResponse } from "./matomo-api";
 export async function fetchTagManagerData(date: Date) {
   const formattedDate = date.toISOString().split("T")[0];
 
-  const url = `https://${process.env.EGR_METRICS_MATOMO_DOMAIN}/index.php?module=API&format=JSON&idSite=86&period=day&date=${formattedDate},${formattedDate}&method=Events.getCategory&filter_limit=100&format_metrics=1&expanded=1&token_auth=${process.env.EGR_METRICS_MATOMO_AUTHENTICATION_TOKEN}&force_api_session=1`;
+  const url = `https://${process.env.EGR_METRICS_SYNC_MATOMO_DOMAIN}/index.php?module=API&format=JSON&idSite=86&period=day&date=${formattedDate},${formattedDate}&method=Events.getCategory&filter_limit=100&format_metrics=1&expanded=1&token_auth=${process.env.EGR_METRICS_SYNC_MATOMO_AUTHENTICATION_TOKEN}&force_api_session=1`;
 
   const response = await fetch(url);
 
--- metrics-sync/nocco.ts
+++ metrics-sync/nocco.ts
@@ -1,9 +1,9 @@
 import { ElterngeldTableSchema } from "./nocco-schema";
 
 export async function createTableRecord(object: ElterngeldTableSchema) {
-  const url = `https://${process.env.EGR_METRICS_NOCO_DOMAIN}:${process.env.EGR_METRICS_NOCO_PORT}/api/v2/tables/${process.env.EGR_METRICS_NOCO_PROJECT_ID}/records`;
+  const url = `https://${process.env.EGR_METRICS_SYNC_NOCO_DOMAIN}:${process.env.EGR_METRICS_SYNC_NOCO_PORT}/api/v2/tables/${process.env.EGR_METRICS_SYNC_NOCO_PROJECT_ID}/records`;
 
-  if (process.env.EGR_METRICS_DRY_RUN) {
+  if (process.env.EGR_METRICS_SYNC_DRY_RUN) {
     // eslint-disable-next-line no-console
     return new Promise(() => console.log("Request skipped in dry run."));
   } else {
@@ -11,7 +11,7 @@ export async function createTableRecord(object: ElterngeldTableSchema) {
       body: JSON.stringify(object),
       method: "POST",
       headers: {
-        "xc-token": process.env.EGR_METRICS_NOCO_AUTHENTICATION_TOKEN,
+        "xc-token": process.env.EGR_METRICS_SYNC_NOCO_AUTHENTICATION_TOKEN,
         "Content-Type": "application/json",
       },
     });

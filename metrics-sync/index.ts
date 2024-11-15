bb27260fb29999bd6b463d89564a99e83fde456c
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2024-11-15 09:42:34 +0100

    feat(metabase): add support for date arguments
    Allows specifying a date as an argument,
    simplifying the use of the script for migrating
    data from past dates.

--- metrics-sync/index.ts
+++ metrics-sync/index.ts
@@ -2,16 +2,21 @@ import {
   fetchMetadataInformation,
   fetchEventsInformation,
 } from "./matomo/matomo";
+
 import { createTableRecord } from "./noco/noco";
 import { ElterngeldTableSchema } from "./noco/noco-db-schema";
 
-const formattedDate = new Date().toISOString().split("T")[0];
+const date = process.argv.slice(2)[0];
 
-const metadataInformation = await fetchMetadataInformation(formattedDate);
-const eventsInformation = await fetchEventsInformation(formattedDate);
+if (!/^\d{4}-\d{2}-\d{2}$/.test(date)) {
+  throw new Error(`Expected date to be in format YYYY-MM-DD but was ${date}`);
+}
+
+const metadataInformation = await fetchMetadataInformation(date);
+const eventsInformation = await fetchEventsInformation(date);
 
 const record: ElterngeldTableSchema = {
-  Datum: formattedDate,
+  Datum: date,
 
   Partnerschaftlichkeit: eventsInformation.partnerschaftlichkeit,
   AnzahlFeedbackHilfreich: eventsInformation.hilfreichesFeedback,

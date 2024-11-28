a91988ac2708a7134ce2ede98480f8c9629c54c5
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2024-11-28 14:25:50 +0100

    feat(feedback): show feedback form in invalid state as well

--- src/components/pages/RechnerPlanerPage.tsx
+++ src/components/pages/RechnerPlanerPage.tsx
@@ -108,7 +108,7 @@ export default function RechnerPlanerPage() {
     }
   }
 
-  const showFeedbackForm = istPlanGueltig && hasChanges && !submitted;
+  const showFeedbackForm = hasChanges && !submitted;
   const defaultFeedbackValues = {
     obstacle: getTrackedObstacle(),
     ease: getTrackedEase(),

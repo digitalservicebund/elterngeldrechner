bb7d5873737fe7e27944be63d54719822b744bc4
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2024-11-29 16:57:37 +0100

    refactor(tracking): reduce code duplication

--- tests/cases/tracking.spec.ts
+++ tests/cases/tracking.spec.ts
@@ -14,16 +14,15 @@ import {
   getTrackingVariableFrom,
 } from "@/user-tracking/data-layer";
 
-async function getTrackingVariable(page: Page, name: string) {
-  const dataLayer = await page.evaluate(() => window._mtm!);
-
-  return getTrackingVariableFrom(dataLayer, name);
-}
-
 test("10 monate basis und 2 monate mutterschaftsleistung", async ({ page }) => {
   test.slow();
 
   await page.addInitScript(establishDataLayer);
+
+  await page.goto("./");
+
+  const cookieBanner = new CookieBannerPOM(page);
+  await cookieBanner.consent();
 
   const allgemeineAngabenPage = await new AllgemeineAngabenPOM(page);
   await allgemeineAngabenPage.goto();
@@ -49,7 +48,6 @@ test("10 monate basis und 2 monate mutterschaftsleistung", async ({ page }) => {
   await variantenPage.submit();
 
   const rechnerUndPlaner = new RechnerPlanerPOM(page);
-
   await rechnerUndPlaner.waehleOption(3, "Basis");
   await rechnerUndPlaner.waehleOption(4, "Basis");
   await rechnerUndPlaner.waehleOption(5, "Basis");
@@ -60,6 +58,7 @@ test("10 monate basis und 2 monate mutterschaftsleistung", async ({ page }) => {
   await rechnerUndPlaner.waehleOption(10, "Basis");
   await rechnerUndPlaner.waehleOption(11, "Basis");
   await rechnerUndPlaner.waehleOption(12, "Basis");
+
   await rechnerUndPlaner.submit();
 
   expect(await getTrackingVariable(page, "geplante-monate")).toEqual(12);
@@ -67,56 +66,6 @@ test("10 monate basis und 2 monate mutterschaftsleistung", async ({ page }) => {
 });
 
 test("paar das mutterschaftsleistung und bonus nimmt", async ({ page }) => {
-  test.slow();
-
-  await page.addInitScript(establishDataLayer);
-
-  const allgemeineAngabenPage = new AllgemeineAngabenPOM(page);
-  await allgemeineAngabenPage.goto();
-  await allgemeineAngabenPage.setElternteile(2);
-  await allgemeineAngabenPage.setNameElternteil1("Jane");
-  await allgemeineAngabenPage.setNameElternteil2("John");
-  await allgemeineAngabenPage.setMutterschaftsleistungen(true);
-  await allgemeineAngabenPage.setMutterschaftsleistungenWer("Jane");
-  await allgemeineAngabenPage.submit();
-
-  const nachwuchsPage = new NachwuchsPOM(page);
-  await nachwuchsPage.setGeburtsdatum("02.02.2025");
-  await nachwuchsPage.setAnzahlKinder(1);
-  await nachwuchsPage.submit();
-
-  const erwerbstaetigkeitPage = new ErwerbstaetigkeitPOM(page, {
-    elternteile: ["Jane", "John"],
-  });
-  await erwerbstaetigkeitPage.setErwerbstaetig(false, 1);
-  await erwerbstaetigkeitPage.setErwerbstaetig(false, 2);
-  await erwerbstaetigkeitPage.submit();
-
-  const einkommenPage = new EinkommenPOM(page);
-  await einkommenPage.setGesamteinkommenUeberschritten(false);
-  await einkommenPage.submit();
-
-  const variantenPage = new VariantenPOM(page);
-  await variantenPage.submit();
-
-  const rechnerUndPlaner = new RechnerPlanerPOM(page, {
-    elternteile: ["Jane", "John"],
-  });
-  await rechnerUndPlaner.zeigeMehrLebensmonateAn();
-  await rechnerUndPlaner.waehleOption(1, "Plus", "John");
-  await rechnerUndPlaner.waehleOption(2, "Plus", "John");
-  await rechnerUndPlaner.waehleOption(4, "Bonus", "Jane");
-  await rechnerUndPlaner.gebeEinkommenAn(4, 1500, "Jane");
-  await rechnerUndPlaner.gebeEinkommenAn(4, 1500, "John");
-  await rechnerUndPlaner.gebeEinkommenAn(5, 1500, "Jane");
-  await rechnerUndPlaner.gebeEinkommenAn(5, 1500, "John");
-  await rechnerUndPlaner.submit();
-
-  expect(await getTrackingVariable(page, "geplante-monate")).toEqual(8);
-  expect(await getTrackingVariable(page, "aenderungen-am-plan")).toEqual(3);
-});
-
-test("feedback in der planung", async ({ page }) => {
   test.slow();
 
   await page.addInitScript(establishDataLayer);
@@ -156,12 +105,30 @@ test("feedback in der planung", async ({ page }) => {
   const rechnerUndPlaner = new RechnerPlanerPOM(page, {
     elternteile: ["Jane", "John"],
   });
-
-  await rechnerUndPlaner.zeigeMehrLebensmonateAn();
   await rechnerUndPlaner.waehleOption(1, "Plus", "John");
   await rechnerUndPlaner.waehleOption(2, "Plus", "John");
-  await rechnerUndPlaner.waehleOption(3, "Basis", "Jane");
-  await rechnerUndPlaner.waehleOption(3, "Basis", "John");
+  await rechnerUndPlaner.waehleOption(4, "Bonus", "Jane");
+  await rechnerUndPlaner.gebeEinkommenAn(4, 1500, "Jane");
+  await rechnerUndPlaner.gebeEinkommenAn(4, 1500, "John");
+  await rechnerUndPlaner.gebeEinkommenAn(5, 1500, "Jane");
+  await rechnerUndPlaner.gebeEinkommenAn(5, 1500, "John");
+  await rechnerUndPlaner.submit();
+
+  expect(await getTrackingVariable(page, "geplante-monate")).toEqual(8);
+  expect(await getTrackingVariable(page, "aenderungen-am-plan")).toEqual(3);
+});
+
+test("feedback in der planung", async ({ page }) => {
+  test.slow();
+
+  await page.addInitScript(establishDataLayer);
+
+  await page.goto("./");
+
+  const cookieBanner = new CookieBannerPOM(page);
+  await cookieBanner.consent();
+
+  await fastForwardToPlaner(page);
 
   const feedbackForm = new FeedbackPOM(page);
   await feedbackForm.waehleEase(2);
@@ -187,42 +154,7 @@ test("feedback in der planung wird nur ein mal abgefragt", async ({ page }) => {
   const cookieBanner = new CookieBannerPOM(page);
   await cookieBanner.consent();
 
-  const allgemeineAngabenPage = new AllgemeineAngabenPOM(page);
-  await allgemeineAngabenPage.setElternteile(2);
-  await allgemeineAngabenPage.setNameElternteil1("Jane");
-  await allgemeineAngabenPage.setNameElternteil2("John");
-  await allgemeineAngabenPage.setMutterschaftsleistungen(true);
-  await allgemeineAngabenPage.setMutterschaftsleistungenWer("Jane");
-  await allgemeineAngabenPage.submit();
-
-  const nachwuchsPage = new NachwuchsPOM(page);
-  await nachwuchsPage.setGeburtsdatum("02.02.2025");
-  await nachwuchsPage.setAnzahlKinder(1);
-  await nachwuchsPage.submit();
-
-  const erwerbstaetigkeitPage = new ErwerbstaetigkeitPOM(page, {
-    elternteile: ["Jane", "John"],
-  });
-  await erwerbstaetigkeitPage.setErwerbstaetig(false, 1);
-  await erwerbstaetigkeitPage.setErwerbstaetig(false, 2);
-  await erwerbstaetigkeitPage.submit();
-
-  const einkommenPage = new EinkommenPOM(page);
-  await einkommenPage.setGesamteinkommenUeberschritten(false);
-  await einkommenPage.submit();
-
-  const variantenPage = new VariantenPOM(page);
-  await variantenPage.submit();
-
-  const rechnerUndPlaner = new RechnerPlanerPOM(page, {
-    elternteile: ["Jane", "John"],
-  });
-
-  await rechnerUndPlaner.zeigeMehrLebensmonateAn();
-  await rechnerUndPlaner.waehleOption(1, "Plus", "John");
-  await rechnerUndPlaner.waehleOption(2, "Plus", "John");
-  await rechnerUndPlaner.waehleOption(3, "Basis", "Jane");
-  await rechnerUndPlaner.waehleOption(3, "Basis", "John");
+  const rechnerUndPlaner = await fastForwardToPlaner(page);
 
   const feedbackForm = new FeedbackPOM(page);
   await feedbackForm.waehleEase(2);
@@ -249,6 +181,29 @@ test("feedback wird nicht ohne consent angezeigt", async ({ page }) => {
 
   await page.goto("./");
 
+  await fastForwardToPlaner(page);
+
+  const feedbackForm = new FeedbackPOM(page);
+  expect(await feedbackForm.easeQuestion.isVisible()).toBeFalsy();
+});
+
+async function getTrackingVariable(page: Page, name: string) {
+  const dataLayer = await page.evaluate(() => window._mtm!);
+
+  return getTrackingVariableFrom(dataLayer, name);
+}
+
+async function fastForwardToPlaner(page: Page) {
+  await fastForwardAllgemeineAngaben(page);
+  await fastForwardNachwuchs(page);
+  await fastForwardErwerbstaetigkeit(page);
+  await fastForwardEinkommen(page);
+  await fastForwardVarianten(page);
+
+  return await fastForwardPlanung(page);
+}
+
+async function fastForwardAllgemeineAngaben(page: Page) {
   const allgemeineAngabenPage = new AllgemeineAngabenPOM(page);
   await allgemeineAngabenPage.setElternteile(2);
   await allgemeineAngabenPage.setNameElternteil1("Jane");
@@ -256,26 +211,36 @@ test("feedback wird nicht ohne consent angezeigt", async ({ page }) => {
   await allgemeineAngabenPage.setMutterschaftsleistungen(true);
   await allgemeineAngabenPage.setMutterschaftsleistungenWer("Jane");
   await allgemeineAngabenPage.submit();
+}
 
+async function fastForwardNachwuchs(page: Page) {
   const nachwuchsPage = new NachwuchsPOM(page);
   await nachwuchsPage.setGeburtsdatum("02.02.2025");
   await nachwuchsPage.setAnzahlKinder(1);
   await nachwuchsPage.submit();
+}
 
+async function fastForwardErwerbstaetigkeit(page: Page) {
   const erwerbstaetigkeitPage = new ErwerbstaetigkeitPOM(page, {
     elternteile: ["Jane", "John"],
   });
   await erwerbstaetigkeitPage.setErwerbstaetig(false, 1);
   await erwerbstaetigkeitPage.setErwerbstaetig(false, 2);
   await erwerbstaetigkeitPage.submit();
+}
 
+async function fastForwardEinkommen(page: Page) {
   const einkommenPage = new EinkommenPOM(page);
   await einkommenPage.setGesamteinkommenUeberschritten(false);
   await einkommenPage.submit();
+}
 
+async function fastForwardVarianten(page: Page) {
   const variantenPage = new VariantenPOM(page);
   await variantenPage.submit();
+}
 
+async function fastForwardPlanung(page: Page) {
   const rechnerUndPlaner = new RechnerPlanerPOM(page, {
     elternteile: ["Jane", "John"],
   });
@@ -286,6 +251,5 @@ test("feedback wird nicht ohne consent angezeigt", async ({ page }) => {
   await rechnerUndPlaner.waehleOption(3, "Basis", "Jane");
   await rechnerUndPlaner.waehleOption(3, "Basis", "John");
 
-  const feedbackForm = new FeedbackPOM(page);
-  expect(await feedbackForm.easeQuestion.isVisible()).toBeFalsy();
-});
+  return rechnerUndPlaner;
+}

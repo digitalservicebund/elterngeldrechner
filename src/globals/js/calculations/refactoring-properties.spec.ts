fbc723c161dc1e14cad67cb3e881c436c58d81ac
Thore Strassburg <thore@strassburg.me>
2024-12-17 17:59:37 +0100

    test: add tests for refactoring properties of the calculation
    Please read the included documentation for further details.

---
+++ src/globals/js/calculations/refactoring-properties.spec.ts
@@ -0,0 +1,467 @@
+import assert from "assert";
+import { describe, it } from "vitest";
+import {
+  assert as assertProperty,
+  property,
+  boolean as arbitraryBoolean,
+  date as arbitraryDate,
+  constantFrom as arbitraryConstantFrom,
+  array as arbitraryArray,
+  integer as arbitraryInteger,
+  record as arbitraryRecord,
+  tuple as arbitraryTuple,
+  constant as arbitraryConstant,
+  type Arbitrary,
+} from "fast-check";
+import Big from "big.js";
+import { EgrCalculation } from "./egr-calculation";
+import {
+  Einkommen,
+  ElternGeldArt,
+  ErwerbsArt,
+  ErwerbsTaetigkeit,
+  ErwerbsZeitraumLebensMonat,
+  FinanzDaten,
+  KassenArt,
+  KinderFreiBetrag,
+  MischEkTaetigkeit,
+  MutterschaftsLeistung,
+  PersoenlicheDaten,
+  PlanungsDaten,
+  RentenArt,
+  SteuerKlasse,
+  UnterstuetzteLohnsteuerjahre,
+  YesNo,
+  type Kind,
+  type Lohnsteuerjahr,
+} from "./model";
+import {
+  EgrCalculation as OriginalEgrCalculation,
+  FinanzDaten as OriginalFinanzDaten,
+  ErwerbsZeitraumLebensMonat as OriginalErwerbsZeitraumLebensMonat,
+  Big as OriginalBig,
+} from "original-rechner";
+
+/**
+ * The test coverage for the Elterngeld calculation is spare, incomplete and of
+ * low quality. This makes it incredibly hard to touch the code without the
+ * paralyzing fear to break anything unnoticed.
+ * Therefore, these property tests create a remedy. Having a copy of the
+ * "original"/legacy calculation (the last state of untouched code), the tested
+ * main property is stupid simple: the refactored code should always calculate
+ * the exact same result.
+ *
+ * This does not improve the test coverage from the perspective of a truly
+ * correct calculation. But it allows to do certain kind of refactorings without
+ * changing the output. That value can not be underestimated.
+ * Anyhow, this approach has some limits. Despite calling the two algorithms for
+ * comparison, the test has two important tasks. Generating arbitrary input data
+ * and compare the output data of both algorithms.
+ *
+ * The arbitrary input data SHOULD be generated as much as possible using primitive
+ * types (raw). These then become composed and mapped to the actual input data
+ * types for the algorithms. This is partially necessary to satisfy the type
+ * system (class instances) in general, but also required to allow input data
+ * refactoring. The composition and mapping logic must be as trivial as possible
+ * to prevent undetected mistakes.
+ * Arbitrary input data MUST be always be generated in full circumference to
+ * satisfy the original codes interface. The refactoring CAN always easily
+ * ignore/remove any input data securely. Anyhow, mutating the input schema MUST
+ * be done with great care, following the rules of trivial composition above.
+ * There MUST be no logic that tries to resolve cross input data relationships.
+ * Such relationships are a flaw of the original algorithm and should not be
+ * compensated for, introducing security holes into this test.
+ * The input data SHOULD be as dump and arbitrary as possible. Though, it might
+ * make sense to limit certain arbitrary factors to the range or realistic data
+ * (e.g. 10.000.000 expected children). This helps to keep the number of
+ * test runs more focused on "meaningful" cases. This is especially the case
+ * having the lack of proper type-driven interface design.
+ *
+ * The output data MUST be compared in the most reliable way possible. In best
+ * case it SHOULD not transform the output data at all to avoid unrefracting
+ * issues.
+ * Also the algorithm to compare the output data SHOULD be based on
+ * a solid library or platform native functionality. This MIGHT be weaken if
+ * absolutely necessary to allow a certain refactoring, but MUST be done with
+ * great care.
+ * Especially the simplification of the output format during refactoring MUST be
+ * done purposefully.
+ *
+ * In case there is the same "thing" (usually a class) that is called the same
+ * between both algorithm, but is no more the same or can't be the same for the
+ * type-system, the types of the original calculation get an alias name with
+ * `original` as prefix. The same goes for data transformation functions which
+ * act on these types. For simplicity, any other type (including the same named
+ * types in the refactored code) remain as they are.
+ *
+ * The number of runs (`numRuns`) MIGHT be adapted during actual refactoring
+ * work to shorten the feedback cycles. In the end, a refactoring MUST be
+ * verified using the original configuration to gain enough confidence (i.e.
+ * 1.000.000 runs, which take about )
+ */
+describe.only("tests to verify properties during refactoring", () => {
+  it("calculates the same result the original/legacy implementation", () => {
+    assertProperty(
+      property(
+        arbitraryPersoenlicheDatenRaw(),
+        arbitraryFinanzdatenRaw(),
+        arbitraryPlanungsdatenRaw(),
+        arbitraryLohnsteuerjahr(),
+        (
+          persoenlicheDatenRaw,
+          finanzdatenRaw,
+          planungsdatenRaw,
+          lohnsteuerjahr,
+        ) => {
+          const result = new EgrCalculation().calculateElternGeld(
+            {
+              persoenlicheDaten: persoenlicheDatenFrom(persoenlicheDatenRaw),
+              finanzDaten: finanzDatenFrom(finanzdatenRaw),
+              planungsDaten: planungsDatenFrom(planungsdatenRaw),
+            },
+            lohnsteuerjahr,
+          );
+
+          const originalResult =
+            new OriginalEgrCalculation().calculateElternGeld(
+              {
+                persoenlicheDaten: persoenlicheDatenFrom(persoenlicheDatenRaw),
+                finanzDaten: originalFinanzDatenFrom(finanzdatenRaw),
+                planungsDaten: planungsDatenFrom(planungsdatenRaw),
+              },
+              lohnsteuerjahr,
+            );
+
+          return assert.deepStrictEqual(
+            transformAllBigsToNumbersRecursively(result),
+            transformAllBigsToNumbersRecursively(originalResult),
+          );
+        },
+      ),
+      { numRuns: 1000000, endOnFailure: true },
+    );
+  });
+});
+
+/**
+ * Unfortunately, without "neutralizing" Big numbers, the assertion that two
+ * data points that include instances of Big will always fail. This is happens,
+ * because the original calculation has its own Big dependency bundled.
+ * Anyhow, it is also useful to neutralize the usage of Big for refactoring
+ * purposes. So the first case might become obsolete.
+ */
+function transformAllBigsToNumbersRecursively(data: unknown): unknown {
+  if (data instanceof Big || data instanceof OriginalBig) {
+    return data.toNumber();
+  } else if (typeof data === "object" && data !== null) {
+    return Object.fromEntries(
+      Object.entries(data).map(([key, value]) => [
+        key,
+        transformAllBigsToNumbersRecursively(value),
+      ]),
+    );
+  } else if (Array.isArray(data)) {
+    return data.map(transformAllBigsToNumbersRecursively);
+  } else {
+    return data;
+  }
+}
+
+function persoenlicheDatenFrom(data: PersoenlicheDatenRaw): PersoenlicheDaten {
+  const persoenlicheDaten = new PersoenlicheDaten(
+    data.wahrscheinlichesGeburtsdatum,
+  );
+  persoenlicheDaten.anzahlKuenftigerKinder = data.anzahlKuenftigerKinder;
+  persoenlicheDaten.sindSieAlleinerziehend = yesNoFrom(
+    data.sindSieAlleinerziehend,
+  );
+  persoenlicheDaten.etVorGeburt = data.erwerbsartVorDerGeburt;
+  persoenlicheDaten.etNachGeburt = yesNoFrom(data.erwerbstaetigNachDerGeburt);
+  persoenlicheDaten.kinder.push(...data.kinder.map(kindFrom));
+  return persoenlicheDaten;
+}
+
+function originalFinanzDatenFrom(data: FinanzdatenRaw): OriginalFinanzDaten {
+  const finanzdaten = new OriginalFinanzDaten();
+  finanzdaten.bruttoEinkommen = new Einkommen(data.bruttoeinkommen);
+  finanzdaten.zahlenSieKirchenSteuer = yesNoFrom(data.zahlenSieKirchensteuer);
+  finanzdaten.kinderFreiBetrag = data.kinderfreibetrag;
+  finanzdaten.steuerKlasse = data.steuerklasse;
+  finanzdaten.kassenArt = data.kassenart;
+  finanzdaten.rentenVersicherung = data.rentenversicherung;
+  finanzdaten.splittingFaktor = data.splittingfaktor;
+  finanzdaten.mischEinkommenTaetigkeiten = data.mischEinkommenTaetigkeiten.map(
+    mischEkTaetigkeitFrom,
+  );
+  finanzdaten.erwerbsZeitraumLebensMonatList =
+    data.erwerbsZeitraumLebensmonatList.map(
+      originalErwerbsZeitraumLebensMonatFrom,
+    );
+  return finanzdaten;
+}
+
+function finanzDatenFrom(data: FinanzdatenRaw): FinanzDaten {
+  const finanzdaten = new FinanzDaten();
+  finanzdaten.bruttoEinkommen = new Einkommen(data.bruttoeinkommen);
+  finanzdaten.zahlenSieKirchenSteuer = yesNoFrom(data.zahlenSieKirchensteuer);
+  finanzdaten.kinderFreiBetrag = data.kinderfreibetrag;
+  finanzdaten.steuerKlasse = data.steuerklasse;
+  finanzdaten.kassenArt = data.kassenart;
+  finanzdaten.rentenVersicherung = data.rentenversicherung;
+  finanzdaten.splittingFaktor = data.splittingfaktor;
+  finanzdaten.mischEinkommenTaetigkeiten = data.mischEinkommenTaetigkeiten.map(
+    mischEkTaetigkeitFrom,
+  );
+  finanzdaten.erwerbsZeitraumLebensMonatList =
+    data.erwerbsZeitraumLebensmonatList.map(erwerbsZeitraumLebensMonatFrom);
+  return finanzdaten;
+}
+
+function kindFrom(data: KindRaw, index: number): Kind {
+  return {
+    nummer: index + 2,
+    geburtsdatum: data.geburtsdatum,
+    istBehindert: data.istBehindert,
+  };
+}
+
+function planungsDatenFrom(data: PlanungsdatenRaw): PlanungsDaten {
+  const planungsdaten = new PlanungsDaten(
+    data.alleinerziehend,
+    data.erwerbsstatus,
+    data.partnerbonus,
+    data.mutterschaftsleistungen,
+  );
+  planungsdaten.planung = data.planung;
+  return planungsdaten;
+}
+
+function mischEkTaetigkeitFrom(data: MischEkTaetigkeitRaw): MischEkTaetigkeit {
+  const taetigkeit = new MischEkTaetigkeit(false);
+  taetigkeit.erwerbsTaetigkeit = data.erwerbstaetigkeit;
+  taetigkeit.bruttoEinkommenDurchschnitt = Big(
+    data.bruttoeinkommenDurchschnitt,
+  );
+  taetigkeit.bruttoEinkommenDurchschnittMidi = Big(
+    data.bruttoeinkommenDurchschnittMidi,
+  );
+  taetigkeit.bemessungsZeitraumMonate = data.bemessungszeitraumMonate;
+  taetigkeit.rentenVersicherungsPflichtig = yesNoFrom(
+    data.rentenversicherungspflichtig,
+  );
+  taetigkeit.krankenVersicherungsPflichtig = yesNoFrom(
+    data.krankenversicherungspflichtig,
+  );
+  taetigkeit.arbeitslosenVersicherungsPflichtig = yesNoFrom(
+    data.arbeitslosenversicherungspflichtig,
+  );
+  return taetigkeit;
+}
+
+function originalErwerbsZeitraumLebensMonatFrom(
+  data: ErwerbsZeitraumLebensMonatRaw,
+): OriginalErwerbsZeitraumLebensMonat {
+  return new OriginalErwerbsZeitraumLebensMonat(
+    data.vonLebensmonat,
+    data.bisLebensmonat,
+    new Einkommen(data.bruttoProMonat),
+  );
+}
+
+function erwerbsZeitraumLebensMonatFrom(
+  data: ErwerbsZeitraumLebensMonatRaw,
+): ErwerbsZeitraumLebensMonat {
+  return new ErwerbsZeitraumLebensMonat(
+    data.vonLebensmonat,
+    data.bisLebensmonat,
+    new Einkommen(data.bruttoProMonat),
+  );
+}
+
+function yesNoFrom(value: boolean): YesNo {
+  return value ? YesNo.YES : YesNo.NO;
+}
+
+function arbitraryPersoenlicheDatenRaw(): Arbitrary<PersoenlicheDatenRaw> {
+  return arbitraryRecord({
+    anzahlKuenftigerKinder: arbitraryInteger({ min: 1, max: 5 }),
+    wahrscheinlichesGeburtsdatum: arbitraryDate({
+      min: new Date(Math.min(...UnterstuetzteLohnsteuerjahre) - 1, 0, 1),
+      max: new Date(Math.max(...UnterstuetzteLohnsteuerjahre) - 1, 11, 31),
+    }),
+    sindSieAlleinerziehend: arbitraryBoolean(),
+    erwerbsartVorDerGeburt: arbitraryErwerbsArt(),
+    erwerbstaetigNachDerGeburt: arbitraryBoolean(),
+    kinder: arbitraryArray(arbitraryKind(), { maxLength: 7 }),
+  });
+}
+
+type PersoenlicheDatenRaw = {
+  anzahlKuenftigerKinder: number;
+  wahrscheinlichesGeburtsdatum: Date;
+  sindSieAlleinerziehend: boolean;
+  erwerbsartVorDerGeburt: ErwerbsArt;
+  erwerbstaetigNachDerGeburt: boolean;
+  kinder: KindRaw[];
+};
+
+function arbitraryFinanzdatenRaw(): Arbitrary<FinanzdatenRaw> {
+  return arbitraryRecord({
+    bruttoeinkommen: arbitraryBruttoeinkommen(),
+    zahlenSieKirchensteuer: arbitraryBoolean(),
+    kinderfreibetrag: arbitraryKinderfreibetrag(),
+    steuerklasse: arbitrarySteuerklasse(),
+    kassenart: arbitraryKassenart(),
+    rentenversicherung: arbitraryRentenart(),
+    splittingfaktor: arbitrarySplittingfaktor(),
+    mischEinkommenTaetigkeiten: arbitraryArray(
+      arbitraryMischEkTaetigkeitRaw(),
+      {
+        maxLength: 10,
+      },
+    ),
+    erwerbsZeitraumLebensmonatList: arbitraryArray(
+      arbitraryErwerbszeitraumLebensmonat(),
+      { maxLength: 10 },
+    ),
+  });
+}
+
+type FinanzdatenRaw = {
+  bruttoeinkommen: number;
+  zahlenSieKirchensteuer: boolean;
+  kinderfreibetrag: KinderFreiBetrag;
+  steuerklasse: SteuerKlasse;
+  kassenart: KassenArt;
+  rentenversicherung: RentenArt;
+  splittingfaktor: number;
+  mischEinkommenTaetigkeiten: MischEkTaetigkeitRaw[];
+  erwerbsZeitraumLebensmonatList: ErwerbsZeitraumLebensMonatRaw[];
+};
+
+function arbitraryPlanungsdatenRaw(): Arbitrary<PlanungsdatenRaw> {
+  return arbitraryRecord({
+    alleinerziehend: arbitraryBoolean(),
+    erwerbsstatus: arbitraryBoolean(),
+    partnerbonus: arbitraryBoolean(),
+    mutterschaftsleistungen: arbitraryMutterschaftsleistungen(),
+    planung: arbitraryArray(arbitraryElterngeldart(), { maxLength: 32 }),
+  });
+}
+
+type PlanungsdatenRaw = {
+  alleinerziehend: boolean;
+  erwerbsstatus: boolean;
+  partnerbonus: boolean;
+  mutterschaftsleistungen: MutterschaftsLeistung;
+  planung: ElternGeldArt[];
+};
+
+function arbitraryLohnsteuerjahr(): Arbitrary<Lohnsteuerjahr> {
+  return arbitraryConstantFrom(...UnterstuetzteLohnsteuerjahre);
+}
+
+function arbitraryMischEkTaetigkeitRaw(): Arbitrary<MischEkTaetigkeitRaw> {
+  return arbitraryRecord({
+    erwerbstaetigkeit: arbitraryErwerbstaetigkeit(),
+    bruttoeinkommenDurchschnitt: arbitraryBruttoeinkommen(),
+    bruttoeinkommenDurchschnittMidi: arbitraryBruttoeinkommen(),
+    bemessungszeitraumMonate: arbitraryArray(arbitraryBoolean(), {
+      minLength: 12,
+      maxLength: 12,
+    }),
+    rentenversicherungspflichtig: arbitraryBoolean(),
+    krankenversicherungspflichtig: arbitraryBoolean(),
+    arbeitslosenversicherungspflichtig: arbitraryBoolean(),
+  });
+}
+
+type MischEkTaetigkeitRaw = {
+  erwerbstaetigkeit: ErwerbsTaetigkeit;
+  bruttoeinkommenDurchschnitt: number;
+  bruttoeinkommenDurchschnittMidi: number;
+  bemessungszeitraumMonate: boolean[];
+  rentenversicherungspflichtig: boolean;
+  krankenversicherungspflichtig: boolean;
+  arbeitslosenversicherungspflichtig: boolean;
+};
+
+function arbitraryErwerbszeitraumLebensmonat(): Arbitrary<ErwerbsZeitraumLebensMonatRaw> {
+  return arbitraryInteger({
+    min: 1,
+    max: 32,
+  })
+    .chain((vonLebensmonat) =>
+      arbitraryTuple(
+        arbitraryConstant(vonLebensmonat),
+        arbitraryInteger({ min: vonLebensmonat, max: 32 }),
+        arbitraryBruttoeinkommen(),
+      ),
+    )
+    .map(([vonLebensmonat, bisLebensmonat, bruttoProMonat]) => ({
+      vonLebensmonat,
+      bisLebensmonat,
+      bruttoProMonat,
+    }));
+}
+
+type ErwerbsZeitraumLebensMonatRaw = {
+  vonLebensmonat: number;
+  bisLebensmonat: number;
+  bruttoProMonat: number;
+};
+
+function arbitraryKind(): Arbitrary<KindRaw> {
+  return arbitraryRecord({
+    geburtsdatum: arbitraryDate({
+      min: new Date(Math.min(...UnterstuetzteLohnsteuerjahre) - 20, 0, 1),
+      max: new Date(Math.max(...UnterstuetzteLohnsteuerjahre) - 1, 11, 31),
+    }),
+    istBehindert: arbitraryBoolean(),
+  });
+}
+
+type KindRaw = {
+  geburtsdatum: Date;
+  istBehindert: boolean;
+};
+
+function arbitraryErwerbsArt(): Arbitrary<ErwerbsArt> {
+  return arbitraryConstantFrom(...Object.values(ErwerbsArt));
+}
+
+function arbitraryMutterschaftsleistungen(): Arbitrary<MutterschaftsLeistung> {
+  return arbitraryConstantFrom(...Object.values(MutterschaftsLeistung));
+}
+
+function arbitraryElterngeldart(): Arbitrary<ElternGeldArt> {
+  return arbitraryConstantFrom(...Object.values(ElternGeldArt));
+}
+
+function arbitraryKinderfreibetrag(): Arbitrary<KinderFreiBetrag> {
+  return arbitraryConstantFrom(...Object.values(KinderFreiBetrag));
+}
+
+function arbitrarySteuerklasse(): Arbitrary<SteuerKlasse> {
+  return arbitraryConstantFrom(...Object.values(SteuerKlasse));
+}
+
+function arbitraryKassenart(): Arbitrary<KassenArt> {
+  return arbitraryConstantFrom(...Object.values(KassenArt));
+}
+
+function arbitraryRentenart(): Arbitrary<RentenArt> {
+  return arbitraryConstantFrom(...Object.values(RentenArt));
+}
+
+function arbitrarySplittingfaktor(): Arbitrary<number> {
+  return arbitraryInteger({ min: 0, max: 2 });
+}
+
+function arbitraryErwerbstaetigkeit(): Arbitrary<ErwerbsTaetigkeit> {
+  return arbitraryConstantFrom(...Object.values(ErwerbsTaetigkeit));
+}
+
+function arbitraryBruttoeinkommen(): Arbitrary<number> {
+  return arbitraryInteger({ min: 0, max: 30000 });
+}

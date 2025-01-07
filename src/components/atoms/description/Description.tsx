a44bdb055d6c0db150287d49ed75748fc0221de1
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2025-01-07 15:48:31 +0100

    Revert "refactor: drop description scss"
    This reverts commit bd0ce8d3ab52035c6513eb282857fbe65f7ef2ac.

--- src/components/atoms/description/Description.tsx
+++ src/components/atoms/description/Description.tsx
@@ -9,7 +9,10 @@ interface Props {
 
 export function Description({ id, error = false, children }: Props) {
   return (
-    <div className={classNames("mt-8 text-14", error && "text-danger")} id={id}>
+    <div
+      className={classNames("egr-info-text", error && "egr-info-text--error")}
+      id={id}
+    >
       {children}
     </div>
   );

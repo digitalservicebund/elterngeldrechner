c9c3d76a7bc5d865494cbce5bebc9279b69bc2b2
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2025-01-14 14:57:08 +0100

    feat: ignore unused data in metabase sync

--- metrics-sync/index.ts
+++ metrics-sync/index.ts
@@ -12,10 +12,17 @@ if (!/^\d{4}-\d{2}-\d{2}$/.test(date)) {
   throw new Error(`Expected date to be in format YYYY-MM-DD but was ${date}`);
 }
 
+// List of events that are not relevant for metabase
+const ignoredEventActionLabels = ["Klick"];
+
 const metadata = await matomo.fetchPageStatistics(date);
 await noco.createElterngeldrechnerMetadataRecord({ Datum: date, ...metadata });
 
 const eventActions = await matomo.fetchEventActions(date);
 const flatElements = flatten(eventActions);
 const rows = flatElements.map((element) => ({ Datum: date, ...element }));
-await noco.createElterngeldrechnerEventRecord(rows);
+const filteredRows = rows.filter(
+  (it) => !ignoredEventActionLabels.includes(it.action_label),
+);
+
+await noco.createElterngeldrechnerEventRecord(filteredRows);

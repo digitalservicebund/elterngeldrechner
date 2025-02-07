d669dfe359c3d8b23636b3a55a0f98453f625f70
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2025-02-07 12:06:11 +0000

    refactor: inline alert stylesheet and drop scss file

--- src/components/molecules/alert/Alert.tsx
+++ src/components/molecules/alert/Alert.tsx
@@ -9,9 +9,14 @@ interface AlertProps {
 
 export function Alert({ headline, children, className }: AlertProps) {
   return (
-    <div className={classNames("egr-alert", className)}>
-      <strong className="egr-alert__title">
-        <Icon className="egr-alert__icon" />
+    <div
+      className={classNames(
+        "d-flex flex-column gap-4 rounded bg-primary-light px-40 py-24",
+        className,
+      )}
+    >
+      <strong className="line-height-1.5">
+        <Icon className="mr-4 mt-4 shrink-0" />
         {headline}
       </strong>
       {children}

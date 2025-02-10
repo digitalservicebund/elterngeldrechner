46a3252e063d7ddc725e25db650808e372faf79a
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2025-02-10 12:41:12 +0000

    refactor: replace scss in custom-select with tw classes

--- src/components/molecules/custom-select/CustomSelect.tsx
+++ src/components/molecules/custom-select/CustomSelect.tsx
@@ -50,26 +50,32 @@ export function CustomSelect<TFieldValues extends FieldValues>({
 }: CustomSelectProps<TFieldValues>) {
   const error = get(errors, name) as FieldError | undefined;
 
+  const customArrows = {
+    backgroundImage: `url("data:image/svg+xml;utf8,<svg fill='black' height='22' viewBox='0 0 24 24' width='22' xmlns='http://www.w3.org/2000/svg'><path d='M12 2.29289L18.3536 8.64645L17.6464 9.35355L12 3.70711L6.35355 9.35355L5.64645 8.64645L12 2.29289Z'/><path d='M12 21.7071L5.64645 15.3536L6.35355 14.6464L12 20.2929L17.6464 14.6464L18.3536 15.3536L12 21.7071Z'/></svg>")`,
+    backgroundRepeat: "no-repeat",
+    backgroundPositionX: "calc(100% - 0.6875rem)",
+    backgroundPositionY: "0.6875rem",
+  };
+
   return (
     <div
       className={classNames(
-        "egr-custom-select",
-        autoWidth && "egr-custom-select--auto-width",
+        "flex w-full justify-between",
+        autoWidth && "w-auto",
       )}
     >
-      <div className="egr-custom-select-question">
-        <label className="egr-custom-select-question__label" htmlFor={name}>
-          {label}
-        </label>
+      <div className="flex flex-col items-start">
+        <label htmlFor={name}>{label}</label>
 
-        <div className="egr-custom-select-question__control">
+        <div className="relative w-full min-w-max">
           <select
             {...register(name, registerOptions)}
             className={classNames(
-              "egr-custom-select-question__input",
-              error && "egr-custom-select-question__input--error",
-              "focus:!outline focus:!outline-2 focus:!outline-primary",
+              "mt-16 min-w-max appearance-none border border-solid border-grey-dark bg-white !px-16 py-8 !pr-56",
+              "focus:!outline focus:!outline-2 focus:!outline-primary disabled:cursor-default",
+              error && "border-danger",
             )}
+            style={customArrows}
             id={name}
             disabled={disabled}
             aria-invalid={!!error}
@@ -78,7 +84,7 @@ export function CustomSelect<TFieldValues extends FieldValues>({
             {...aria}
           >
             <option
-              className="egr-custom-select-question__option"
+              className="bg-white px-16 py-8"
               value=""
               disabled={required}
               hidden={required}
@@ -89,7 +95,7 @@ export function CustomSelect<TFieldValues extends FieldValues>({
             {options.map((option) => (
               <option
                 key={option.value}
-                className="egr-custom-select-question__option"
+                className="bg-white px-16 py-8"
                 value={option.value}
                 hidden={option.hidden}
               >

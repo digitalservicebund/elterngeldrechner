58f44522f0d56113abbc4b3b390253b9bf555d28
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2025-02-11 10:31:06 +0000

    refactor: replace custom-checkbox scss with tailwind

--- src/components/molecules/custom-checkbox/CustomCheckbox.tsx
+++ src/components/molecules/custom-checkbox/CustomCheckbox.tsx
@@ -49,30 +49,42 @@ export function CustomCheckbox<TFieldValues extends FieldValues>({
     }
   };
 
+  const checkboxClasses = {
+    defaultBorderState:
+      "before:absolute before:left-0 before:top-0 before:size-32 before:border before:border-solid before:border-primary before:bg-white before:content-['']",
+    defaultMarkerState:
+      "after:absolute after:left-8 after:top-8 after:size-16 after:content-['']",
+
+    focusBorderState:
+      "peer-focus:before:outline peer-focus:before:outline-2 peer-focus:before:outline-primary",
+
+    activeMarkerState: "peer-checked:after:bg-primary",
+    activeBorderState: "peer-checked:before:border-primary",
+
+    errorClasses: hasError
+      ? "before:border-danger peer-checked:after:bg-danger text-danger"
+      : null,
+  };
+
+  const allCheckboxClasses = Object.values(checkboxClasses).join(" ");
+
   return (
-    <div className="egr-custom-checkbox">
+    <div className="flex justify-between p-8 pl-0">
       <div>
         <input
           {...register(name, registerOptions)}
           type="checkbox"
           className={classNames(
-            "egr-custom-checkbox__input",
-            hasError && "egr-custom-checkbox__input--error",
+            "peer absolute opacity-0",
+            hasError && "border-danger",
           )}
           id={name}
           onClick={onClick}
           aria-invalid={hasError}
-          aria-describedby={
-            errorMessage && `${name}-error`
-              ? errorMessage && `${name}-error`
-              : undefined
-          }
+          aria-describedby={errorMessage ? `${name}-error` : undefined}
         />
         <label
-          className={classNames(
-            "egr-custom-checkbox__label",
-            hasError && "egr-custom-checkbox__label--error",
-          )}
+          className={classNames("relative pl-40", allCheckboxClasses)}
           htmlFor={name}
         >
           {label}

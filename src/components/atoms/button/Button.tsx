735943da4f6164789575d099a91cb14116def60a
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2025-02-12 12:24:11 +0000

    refactor: move tailwind classes over to button component

--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -34,18 +34,34 @@ export const Button = forwardRef(function Button<
   }: Props<TFieldValues>,
   ref?: ForwardedRef<HTMLButtonElement>,
 ) {
+  // The [@media(hover:hover)]:hover is used to ensure that the hover style is only applied
+  // on devices that support hover, preventing it from being applied on touch devices after
+  // clicking the button.
+
+  const twClasses = {
+    base: "border border-solid border-primary text-20 transition-all duration-300",
+    primary:
+      "bg-primary text-white px-24 py-16 [@media(hover:hover)]:hover:bg-white [@media(hover:hover)]:hover:text-primary active:focus:bg-white active:focus:text-primary",
+    secondary:
+      "bg-transparent text-primary px-24 py-16 [@media(hover:hover)]:hover:bg-primary [@media(hover:hover)]:hover:text-white active:focus:bg-primary active:focus:text-white",
+    link: "bg-transparent text-primary p-0 border-none [@media(hover:hover)]:hover:bg-transparent active:focus:outline-none",
+    disabled: "pointer-events-none opacity-50",
+  };
+
+  const appliedClasses = classNames(
+    twClasses.base,
+    className,
+    buttonStyle === "primary" && twClasses.primary,
+    buttonStyle === "secondary" && twClasses.secondary,
+    buttonStyle === "link" && twClasses.link,
+    disabled && twClasses.disabled,
+  );
+
   return (
     <button
       id={id}
       ref={ref}
-      className={classNames(
-        "egr-button",
-        buttonStyle === "primary" && "egr-button--primary",
-        buttonStyle === "secondary" && "egr-button--secondary",
-        buttonStyle === "link" && "egr-button--link",
-        disabled && "egr-button--disabled",
-        className,
-      )}
+      className={appliedClasses}
       type={isSubmitButton ? "submit" : "button"}
       onClick={onClick}
       disabled={disabled}
@@ -55,7 +71,9 @@ export const Button = forwardRef(function Button<
       {!!iconBefore && iconBefore}
       {!!iconBefore && <>&nbsp;</>}
 
-      <span>{label}</span>
+      <span className={classNames(buttonStyle === "link" ? "underline" : null)}>
+        {label}
+      </span>
 
       {!!iconAfter && <>&nbsp;</>}
       {!!iconAfter && iconAfter}

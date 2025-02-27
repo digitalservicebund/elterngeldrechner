7a9d8226632287886ae4c3c3c0458f3e56e37b4e
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2025-02-27 15:17:53 +0100

    fix: timezone issue in date-fns calls

--- src/features/planer/domain/zeitraum/operation/berechneZeitraumFuerLebensmonat.ts
+++ src/features/planer/domain/zeitraum/operation/berechneZeitraumFuerLebensmonat.ts
@@ -1,3 +1,4 @@
+import { utc } from "@date-fns/utc";
 import { addDays, addMonths, subDays } from "date-fns";
 
 import type { Lebensmonatszahl } from "@/features/planer/domain/Lebensmonatszahl";
@@ -13,19 +14,25 @@ export function berechneZeitraumFuerLebensmonat(
 }
 
 const startOfNthLebensmonat = (nth: number) => {
-  return (geburtsdatum: Date) => {
+  return (geburtsdatum: Date): Date => {
     const endOfPreviousLebensmonat = endOfNthLebensmonat(nth - 1)(geburtsdatum);
-    return addDays(endOfPreviousLebensmonat, 1);
+    return addDays(endOfPreviousLebensmonat, 1, {
+      in: utc,
+    });
   };
 };
 
 // reference: https://www.zbfs.bayern.de/familie/elterngeld/lebensmonatsrechner/index.php
 const endOfNthLebensmonat = (nth: number) => {
-  return (geburtsdatum: Date) => {
-    const monthsAdded = addMonths(geburtsdatum, nth);
+  return (geburtsdatum: Date): Date => {
+    const monthsAdded = addMonths(geburtsdatum, nth, {
+      in: utc,
+    });
 
     if (monthsAdded.getUTCDate() === geburtsdatum.getUTCDate()) {
-      return subDays(monthsAdded, 1);
+      return subDays(monthsAdded, 1, {
+        in: utc,
+      });
     } else {
       return monthsAdded;
     }
@@ -77,7 +84,7 @@ if (import.meta.vitest) {
         ])(
           "endOfNthLebensmonat(%i)(%i) -> %i",
           (nth, geburtsdatum, startOfLebensmonat) => {
-            expect(startOfNthLebensmonat(nth)(geburtsdatum)).toStrictEqual(
+            expect(startOfNthLebensmonat(nth)(geburtsdatum)).toEqual(
               startOfLebensmonat,
             );
           },
@@ -86,48 +93,46 @@ if (import.meta.vitest) {
 
       describe("end of n-th Lebensmonat", () => {
         it.each([
-          [1, "2024-02-29", "2024-03-28"],
-          [2, "2024-02-29", "2024-04-28"],
-          [3, "2024-02-29", "2024-05-28"],
-          [4, "2024-02-29", "2024-06-28"],
-          [5, "2024-02-29", "2024-07-28"],
-          [6, "2024-02-29", "2024-08-28"],
-          [7, "2024-02-29", "2024-09-28"],
-          [8, "2024-02-29", "2024-10-28"],
-          [9, "2024-02-29", "2024-11-28"],
-          [10, "2024-02-29", "2024-12-28"],
-          [11, "2024-02-29", "2025-01-28"],
-          [12, "2024-02-29", "2025-02-28"],
-          [13, "2024-02-29", "2025-03-28"],
-          [14, "2024-02-29", "2025-04-28"],
-          [15, "2024-02-29", "2025-05-28"],
-          [46, "2024-02-29", "2027-12-28"],
-          [1, "2024-01-31", "2024-02-29"],
-          [2, "2024-01-31", "2024-03-30"],
-          [3, "2024-01-31", "2024-04-30"],
-          [4, "2024-01-31", "2024-05-30"],
-          [5, "2024-01-31", "2024-06-30"],
-          [6, "2024-01-31", "2024-07-30"],
-          [7, "2024-01-31", "2024-08-30"],
-          [8, "2024-01-31", "2024-09-30"],
-          [9, "2024-01-31", "2024-10-30"],
-          [10, "2024-01-31", "2024-11-30"],
-          [11, "2024-01-31", "2024-12-30"],
-          [12, "2024-01-31", "2025-01-30"],
-          [13, "2024-01-31", "2025-02-28"],
-          [14, "2024-01-31", "2025-03-30"],
-          [15, "2024-01-31", "2025-04-30"],
-          [46, "2024-01-31", "2027-11-30"],
+          [1, new Date("2024-02-29"), new Date("2024-03-28")],
+          [2, new Date("2024-02-29"), new Date("2024-04-28")],
+          [3, new Date("2024-02-29"), new Date("2024-05-28")],
+          [5, new Date("2024-02-29"), new Date("2024-07-28")],
+          [4, new Date("2024-02-29"), new Date("2024-06-28")],
+          [6, new Date("2024-02-29"), new Date("2024-08-28")],
+          [7, new Date("2024-02-29"), new Date("2024-09-28")],
+          [8, new Date("2024-02-29"), new Date("2024-10-28")],
+          [9, new Date("2024-02-29"), new Date("2024-11-28")],
+          [10, new Date("2024-02-29"), new Date("2024-12-28")],
+          [11, new Date("2024-02-29"), new Date("2025-01-28")],
+          [12, new Date("2024-02-29"), new Date("2025-02-28")],
+          [13, new Date("2024-02-29"), new Date("2025-03-28")],
+          [14, new Date("2024-02-29"), new Date("2025-04-28")],
+          [15, new Date("2024-02-29"), new Date("2025-05-28")],
+          [46, new Date("2024-02-29"), new Date("2027-12-28")],
+          [1, new Date("2024-01-31"), new Date("2024-02-29")],
+          [2, new Date("2024-01-31"), new Date("2024-03-30")],
+          [3, new Date("2024-01-31"), new Date("2024-04-30")],
+          [4, new Date("2024-01-31"), new Date("2024-05-30")],
+          [5, new Date("2024-01-31"), new Date("2024-06-30")],
+          [6, new Date("2024-01-31"), new Date("2024-07-30")],
+          [7, new Date("2024-01-31"), new Date("2024-08-30")],
+          [8, new Date("2024-01-31"), new Date("2024-09-30")],
+          [9, new Date("2024-01-31"), new Date("2024-10-30")],
+          [10, new Date("2024-01-31"), new Date("2024-11-30")],
+          [11, new Date("2024-01-31"), new Date("2024-12-30")],
+          [12, new Date("2024-01-31"), new Date("2025-01-30")],
+          [13, new Date("2024-01-31"), new Date("2025-02-28")],
+          [14, new Date("2024-01-31"), new Date("2025-03-30")],
+          [15, new Date("2024-01-31"), new Date("2025-04-30")],
+          [46, new Date("2024-01-31"), new Date("2027-11-30")],
         ])(
           "endOfNthLebensmonat(%i)(%j) -> %j",
           (nth, geburtsdatum, endOfLebensmonat) => {
-            expect(
-              toIso(endOfNthLebensmonat(nth)(new Date(geburtsdatum))),
-            ).toBe(endOfLebensmonat);
+            expect(endOfNthLebensmonat(nth)(geburtsdatum)).toEqual(
+              endOfLebensmonat,
+            );
           },
         );
-
-        const toIso = (date: Date) => date.toISOString().slice(0, 10);
       });
     });
   });

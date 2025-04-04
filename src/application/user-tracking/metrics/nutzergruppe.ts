8b78867221128e1985c0a7922fe083be664d69a2
Thore Strassburg <thore@strassburg.me>
2025-04-04 12:35:21 +0200

    feat(user-tracking): extends Nutzergruppen for werdende Eltern
    The huge majority (>80%) of all users are categorized as "werdende
    Eltern. To gain further insides, this group was split into two.
    
    The implementation of the mapping function has been replace because it
    was not well suited anymore. It was my own implementation, but it was
    not too good. It also uses the date utility functions we use everywhere
    else.
    The testing has been changed to a property based approach for better
    guarantees. It was necessary to provide some convenience interface for
    the data generation to improve the readability and expressiveness of the
    tests.

--- src/application/user-tracking/metrics/nutzergruppe.ts
+++ src/application/user-tracking/metrics/nutzergruppe.ts
@@ -1,40 +1,49 @@
+import { addMonths, subWeeks } from "date-fns";
+import type { Arbitrary } from "fast-check";
 import { setTrackingVariable } from "@/application/user-tracking/core";
 
-enum Nutzergruppe {
-  WERDENDE_ELTERN = "werdende Eltern",
-  JUNGE_ELTERN = "junge Eltern",
-  NACHBEANTRAGENDE_ELTERN = "nachbeantragende Eltern",
-}
-
 export function trackNutzergruppe(birthdate: Date): void {
-  const nutzergruppe = mapDateToNutzergruppe(birthdate);
-  setTrackingVariable(TRACKING_VARIABLE_NAME, nutzergruppe);
+  const nutzergruppe = mapBirthdateToNutzergruppe(birthdate);
+  setTrackingVariable("nutzergruppe", nutzergruppe);
 }
 
-function mapDateToNutzergruppe(birthdate: Date): Nutzergruppe {
+function mapBirthdateToNutzergruppe(birthdate: Date): Nutzergruppe {
   const today = new Date();
-  const dayThreeMonthsAgo = new Date(new Date().setMonth(today.getMonth() - 3));
+  const tenWeeksBeforeBeforeBirth = subWeeks(birthdate, 10);
+  const threeMonthsAfterBirth = addMonths(birthdate, 3);
 
-  const isInFuture = birthdate > today;
-  const wasWithinLastThreeMonths =
-    dayThreeMonthsAgo <= birthdate && birthdate <= today;
-
-  if (isInFuture) {
-    return Nutzergruppe.WERDENDE_ELTERN;
+  if (today < tenWeeksBeforeBeforeBirth) {
+    return Nutzergruppe.WerdendeElternPhase1;
+  } else if (today < birthdate) {
+    return Nutzergruppe.WerdendeElternPhase2;
+  } else if (today < threeMonthsAfterBirth) {
+    return Nutzergruppe.JungeEltern;
   } else {
-    return wasWithinLastThreeMonths
-      ? Nutzergruppe.JUNGE_ELTERN
-      : Nutzergruppe.NACHBEANTRAGENDE_ELTERN;
+    return Nutzergruppe.NachbeantragendeEltern;
   }
 }
 
-const TRACKING_VARIABLE_NAME = "nutzergruppe";
+enum Nutzergruppe {
+  WerdendeElternPhase1 = "werdende Eltern (Phase 1)",
+  WerdendeElternPhase2 = "werdende Eltern (Phase 2)",
+  JungeEltern = "junge Eltern",
+  NachbeantragendeEltern = "nachbeantragende Eltern",
+}
 
 if (import.meta.vitest) {
   const { describe, beforeEach, afterEach, it, expect, vi } = import.meta
     .vitest;
 
-  describe("trackNutzergruppe()", () => {
+  describe("track Nutzergruppe", async () => {
+    const { subDays } = await import("date-fns");
+    const {
+      assert,
+      property,
+      date: arbitraryDate,
+      record: arbitraryRecord,
+      constant,
+    } = await import("fast-check");
+
     beforeEach(async () => {
       vi.useFakeTimers();
 
@@ -48,74 +57,163 @@ if (import.meta.vitest) {
       vi.useRealTimers();
     });
 
-    it("sets the tracking variabe 'nutzergruppe'", () => {
-      trackNutzergruppe(ANY_BIRTHDATE);
+    it("sets the tracking variable 'nutzergruppe'", () => {
+      assert(
+        property(arbitraryBirthdate(), (birthdate) => {
+          vi.mocked(setTrackingVariable).mockReset();
 
-      expect(setTrackingVariable).toHaveBeenCalledTimes(1);
-      expect(setTrackingVariable).toHaveBeenLastCalledWith(
-        "nutzergruppe",
-        expect.anything(),
+          trackNutzergruppe(birthdate);
+
+          expect(setTrackingVariable).toHaveBeenCalledTimes(1);
+          expect(setTrackingVariable).toHaveBeenLastCalledWith(
+            "nutzergruppe",
+            expect.anything(),
+          );
+        }),
       );
     });
 
-    it.each([
-      new Date(2024, 6, 3),
-      new Date(2024, 12, 31),
-      new Date(2025, 6, 2),
-      new Date(2026, 1, 1),
-    ])(
-      "sets the variable to 'werdene Eltern' if the birthdate is in the future (case #$#)",
-      (birthdate) => {
-        vi.setSystemTime(new Date(2024, 6, 2));
+    it("Nutzergruppe is 'werdende Eltern (Phase 1)' if today is earlier than 10 weeks before the birthdate", () => {
+      assert(
+        property(
+          arbitraryParameters(
+            arbitraryBirthdate(),
+            arbitraryToday({
+              max: (birthdate) => subDays(subWeeks(birthdate, 10), 1),
+            }),
+          ),
+          ({ birthdate, today }) => {
+            vi.mocked(setTrackingVariable).mockReset();
+            vi.setSystemTime(today);
 
-        trackNutzergruppe(birthdate);
+            trackNutzergruppe(birthdate);
 
-        expect(setTrackingVariable).toHaveBeenCalledWith(
-          expect.anything(),
-          "werdende Eltern",
-        );
-      },
-    );
+            expect(setTrackingVariable).toHaveBeenCalledWith(
+              expect.anything(),
+              "werdende Eltern (Phase 1)",
+            );
+          },
+        ),
+      );
+    });
 
-    it.each([
-      new Date(2024, 6, 2),
-      new Date(2024, 6, 1),
-      new Date(2024, 4, 5),
-      new Date(2024, 3, 3),
-      new Date(2024, 3, 2),
-    ])(
-      "sets the variable to 'junge Eltern' if the birthdate was within the last three months (case #$#)",
-      (birthdate: Date) => {
-        vi.setSystemTime(new Date(2024, 6, 2));
+    it("Nutzergruppe is 'werdende Eltern (Phase 2)' if today is between the 31st week of pregnancy and date of birth", () => {
+      assert(
+        property(
+          arbitraryParameters(
+            arbitraryBirthdate(),
+            arbitraryToday({
+              min: (birthdate) => subWeeks(birthdate, 10),
+              max: (birthdate) => subDays(birthdate, 1),
+            }),
+          ),
+          ({ birthdate, today }) => {
+            vi.mocked(setTrackingVariable).mockReset();
+            vi.setSystemTime(today);
 
-        trackNutzergruppe(birthdate);
+            trackNutzergruppe(birthdate);
 
-        expect(setTrackingVariable).toHaveBeenCalledWith(
-          expect.anything(),
-          "junge Eltern",
-        );
-      },
-    );
+            expect(setTrackingVariable).toHaveBeenCalledWith(
+              expect.anything(),
+              "werdende Eltern (Phase 2)",
+            );
+          },
+        ),
+      );
+    });
 
-    it.each([
-      new Date(2024, 3, 1),
-      new Date(2024, 2, 28),
-      new Date(2024, 1, 1),
-      new Date(2023, 12, 31),
-    ])(
-      "sets the variable to 'nachbeantragende Eltern' if the birthdate was in the past longer than three months ago (case #$#)",
-      (birthdate: Date) => {
-        vi.setSystemTime(new Date(2024, 6, 2));
+    it("Nutzergruppe is 'junge Eltern' if today is between the date of birth and three months after", () => {
+      assert(
+        property(
+          arbitraryParameters(
+            arbitraryBirthdate(),
+            arbitraryToday({
+              min: (birthdate) => birthdate,
+              max: (birthdate) => subDays(addMonths(birthdate, 3), 1),
+            }),
+          ),
+          ({ birthdate, today }) => {
+            vi.mocked(setTrackingVariable).mockReset();
+            vi.setSystemTime(today);
 
-        trackNutzergruppe(birthdate);
+            trackNutzergruppe(birthdate);
 
-        expect(setTrackingVariable).toHaveBeenCalledWith(
-          expect.anything(),
-          "nachbeantragende Eltern",
-        );
-      },
-    );
+            expect(setTrackingVariable).toHaveBeenCalledWith(
+              expect.anything(),
+              "junge Eltern",
+            );
+          },
+        ),
+      );
+    });
+
+    it("Nutzergruppe is 'nachbeantragende Eltern' if today is later than three months after birth", () => {
+      assert(
+        property(
+          arbitraryParameters(
+            arbitraryBirthdate(),
+            arbitraryToday({
+              min: (birthdate) => addMonths(birthdate, 3),
+            }),
+          ),
+          ({ birthdate, today }) => {
+            vi.mocked(setTrackingVariable).mockReset();
+            vi.setSystemTime(today);
+
+            trackNutzergruppe(birthdate);
+
+            expect(setTrackingVariable).toHaveBeenCalledWith(
+              expect.anything(),
+              "nachbeantragende Eltern",
+            );
+          },
+        ),
+      );
+    });
+
+    /**
+     * Produce arbitrary test parameters by providing a readable interface that
+     * supports multiple parameter to depend on each other. I.e. the arbitrarily
+     * sample birth date is passed into the generation of the date for today.
+     */
+    function arbitraryParameters(
+      birthdate: Arbitrary<Date>,
+      today: ArbitraryRelativeDate,
+    ): Arbitrary<{ birthdate: Date; today: Date }> {
+      return birthdate.chain((birthdate) =>
+        arbitraryRecord({
+          birthdate: constant(birthdate),
+          today: today(birthdate),
+        }),
+      );
+    }
+
+    function arbitraryToday(constraints?: {
+      min?: (birthdate: Date) => Date;
+      max?: (birthdate: Date) => Date;
+    }): ArbitraryRelativeDate {
+      return (birthdate: Date) =>
+        arbitraryDate({
+          min: constraints?.min?.(birthdate),
+          max: constraints?.max?.(birthdate),
+        });
+    }
+
+    type ArbitraryRelativeDate = (date: Date) => Arbitrary<Date>;
+
+    /**
+     * Wrapper around {@link arbitraryDate} to ensure usable dates are produced.
+     * Like no invalid dates and dates in a range that allow arithmetic
+     * operations. For example if the latest date technically representable is
+     * generated, it would no more be possible to subtract any time span from
+     * it.
+     */
+    function arbitraryBirthdate(constraints?: { min?: Date; max?: Date }) {
+      return arbitraryDate({
+        min: constraints?.min ?? new Date("2020-01-01"),
+        max: constraints?.max ?? new Date("2050-01-01"),
+        noInvalidDate: true,
+      });
+    }
   });
-
-  const ANY_BIRTHDATE = new Date();
 }

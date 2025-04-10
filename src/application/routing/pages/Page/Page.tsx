ea100f84db3199aef614cbe45a30f4a536237d58
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2025-04-10 13:15:20 +0200

    fix: scrolling down on page navigation

--- src/application/routing/pages/Page/Page.tsx
+++ src/application/routing/pages/Page/Page.tsx
@@ -1,4 +1,5 @@
 import { ReactNode, useEffect, useId, useRef } from "react";
+import { ScrollRestoration } from "react-router-dom";
 import { Sidebar } from "./Sidebar";
 import { FormStep } from "@/application/routing/formSteps";
 
@@ -10,16 +11,15 @@ type Props = {
 export function Page({ step, children }: Props) {
   const sectionElement = useRef<HTMLElement>(null);
   useEffect(() => {
-    // scroll to top on page navigation
-    // <ScrollNavigation /> does not reliably scroll to top of next page
-    window.scrollTo({ top: 0 });
-    // sectionElement.current?.focus();
+    sectionElement.current?.focus({ preventScroll: true });
   }, []);
 
   const headingIdentifier = useId();
 
   return (
     <div className="page-grid-container print:block">
+      <ScrollRestoration />
+
       <div className="page-grid-sidebar relative min-[1170px]:mr-56 print:hidden">
         <Sidebar currentStep={step} />
       </div>

5ff2fc34ef284b9e8205ad3fbf7ee6de0d48032b
joschka <41898282+github-actions[bot]@users.noreply.github.com>
2025-06-03 08:25:56 +0000

    Automated deployment to GitHub pages (/prototype/planungshilfen-und-pruefbutton | prototype/planungshilfen-und-pruefbutton | cf9e4bea9f3008509e68036664dbe362cb836841)

--- prototype/planungshilfen-und-pruefbutton/include.js
+++ prototype/planungshilfen-und-pruefbutton/include.js
@@ -1,13 +1,9 @@
 (function() {
-  const params = new URL(document.location).searchParams;
-  const toggle = params.get("original");
-  if (toggle === "1") return;
-
-  const el = document.createElement("div");
-  el.innerText = "Testumgebung!";
-  el.className = "ds-preview-warning";
   document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector("body").appendChild(el);
     document.querySelectorAll(".header__container, .breadcrumb, .page-title, .contact-flap, .footer").forEach(el => el.classList.add("ds-disable"));
+    var links = document.querySelectorAll("a[href^=https]");
+    for (var i = 0; i < links.length; i++) {
+      links[i].style.pointerEvents = "none";
+    }
   });
 }());

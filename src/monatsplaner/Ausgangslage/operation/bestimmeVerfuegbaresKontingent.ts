313a03c932dd1d020f74f5521c15669af0d25dcd
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2025-07-16 14:01:30 +0200

    refactor: no need for a function here

--- src/monatsplaner/Ausgangslage/operation/bestimmeVerfuegbaresKontingent.ts
+++ src/monatsplaner/Ausgangslage/operation/bestimmeVerfuegbaresKontingent.ts
@@ -22,20 +22,16 @@ export function bestimmeVerfuegbaresKontingent(
   const { anzahlElternteile, istAlleinerziehend } = ausgangslage;
   const sindMehrereElternteile = anzahlElternteile > 1;
 
-  const anzahlBonusMonate = () => {
-    if (sindMehrereElternteile) {
-      return ANZAHL_BONUS_LEBENSMONATE * 2;
-    } else if (istAlleinerziehend) {
-      return ANZAHL_BONUS_LEBENSMONATE;
-    } else {
-      return 0;
-    }
-  };
+  const anzahlBonusMonate = sindMehrereElternteile
+    ? ANZAHL_BONUS_LEBENSMONATE * 2
+    : istAlleinerziehend
+      ? ANZAHL_BONUS_LEBENSMONATE
+      : 0;
 
   return {
     [Variante.Basis]: anzahlBasisMonate,
     [Variante.Plus]: anzahlPlusMonate,
-    [Variante.Bonus]: anzahlBonusMonate(),
+    [Variante.Bonus]: anzahlBonusMonate,
   };
 }
 

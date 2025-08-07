c177dfb28c7c909262d424018eec45517d7757dd
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2025-08-07 17:53:45 +0200

    feat(planungshilfen): use example selection from planer in example page

--- src/application/routing/pages/BeispielePage.tsx
+++ src/application/routing/pages/BeispielePage.tsx
@@ -1,18 +1,50 @@
+import { useState } from "react";
 import { useNavigate } from "react-router";
 import { Page } from "./Page";
+import { useNavigateWithPlan } from "./useNavigateWithPlan";
 import { Button } from "@/application/components";
+import { composeAusgangslageFuerPlaner } from "@/application/features/abfrageteil/state";
+import { BeispielAuswahl } from "@/application/features/beispiele/component/BeispielAuswahl";
+import { useBeispieleService } from "@/application/features/beispiele/hooks";
+import { useAppStore } from "@/application/redux/hooks";
 import { formSteps } from "@/application/routing/formSteps";
+import { PlanMitBeliebigenElternteilen } from "@/monatsplaner";
 
 export function BeispielePage() {
+  const store = useAppStore();
   const navigate = useNavigate();
-  const navigateToEinkommenPage = () => navigate(formSteps.einkommen.route);
-  const navigateToRechnerUndPlanerPage = () =>
-    navigate(formSteps.rechnerUndPlaner.route);
+
+  const { navigateWithPlanState } = useNavigateWithPlan();
+
+  const navigateToEinkommenPage = async () => {
+    await navigate(formSteps.einkommen.route);
+  };
+  const navigateToRechnerUndPlanerPage = async () => {
+    await navigateWithPlanState(formSteps.rechnerUndPlaner.route, plan);
+  };
+
+  const ausgangslage = composeAusgangslageFuerPlaner(store.getState());
+  const [plan, setPlan] = useState<PlanMitBeliebigenElternteilen>();
+
+  // TODO: Tracking
+
+  const {
+    beschreibungenDerBeispiele,
+    waehleBeispielAus,
+    istBeispielAusgewaehlt,
+  } = useBeispieleService(ausgangslage, setPlan);
 
   return (
     <Page step={formSteps.beispiele}>
       <div className="flex flex-col gap-56">
-        <div></div>
+        <div>
+          <BeispielAuswahl
+            className="mx-[-15px] sm:mx-0"
+            beschreibungenDerBeispiele={beschreibungenDerBeispiele}
+            waehleBeispielAus={waehleBeispielAus}
+            istBeispielAusgewaehlt={istBeispielAusgewaehlt}
+          />
+        </div>
         <div className="flex gap-16">
           <Button
             type="button"

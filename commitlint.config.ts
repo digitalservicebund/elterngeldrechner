1d654d3768219b9b0ab55ccfe0b18008bda35f09
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2025-08-12 11:24:26 +0200

    chore: add body-max-line-length to commitlint config
    The lines of the body should ideally be no longer
    than the header line in order to provide a consistent
    flow of the text.

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -5,5 +5,6 @@ export default {
   rules: {
     "body-case": [RuleConfigSeverity.Error, "always", "sentence-case"],
     "header-max-length": [RuleConfigSeverity.Error, "always", 72],
+    "body-max-line-length": [RuleConfigSeverity.Error, "always", 72],
   },
 } satisfies UserConfig;

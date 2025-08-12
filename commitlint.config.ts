838a595355bcae0b5a7c55091adee4ac140a6fa0
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2025-08-12 11:21:10 +0200

    chore: add header-max-length rule to commitlint config
    Header messages longer than 72 characters cause the github user
    interface to trim the message.

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -4,5 +4,6 @@ export default {
   extends: "@commitlint/config-conventional",
   rules: {
     "body-case": [RuleConfigSeverity.Error, "always", "sentence-case"],
+    "header-max-length": [RuleConfigSeverity.Error, "always", 72],
   },
 } satisfies UserConfig;

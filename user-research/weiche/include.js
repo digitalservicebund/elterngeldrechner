4c2869fd6d29f16bb8fdc37808d256dea9eb64bc
deen13 <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-15 17:28:35 +0000

    Automated undeployment from GitHub Pages (/user-research/weiche/)

--- user-research/weiche/include.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll(".header__container, .breadcrumb, .page-title, .contact-flap, .footer").forEach(el => el.classList.add("ds-disable"));
-    var links = document.querySelectorAll("a[href^=https]");
-    for (var i = 0; i < links.length; i++) {
-      links[i].style.pointerEvents = "none";
-    }
-  });
-}());

35f10fe328ba874692893cac1290af76860399ed
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2025-08-25 16:22:11 +0200

    feat(planungshilfen): make beispiele selectable via input

--- src/application/pages/planungsteil/Beispiele/BeispielePage.tsx
+++ src/application/pages/planungsteil/Beispiele/BeispielePage.tsx
@@ -4,6 +4,7 @@ import { trackMetricsForEinBeispielWurdeAusgewaehlt } from "./tracking";
 import { Button } from "@/application/components";
 import { composeAusgangslageFuerPlaner } from "@/application/features/abfrageteil/state";
 import { BeispielAuswahlbox } from "@/application/features/beispiele/component/BeispielAuswahlbox";
+import { BeispielAuswahlboxBody } from "@/application/features/beispiele/component/BeispielAuswahlboxBody";
 import { BeispielAuswahloptionLegende } from "@/application/features/beispiele/component/BeispielAuswahloptionLegende";
 import { useBeispieleService } from "@/application/features/beispiele/hooks";
 import { Page } from "@/application/pages/Page";
@@ -14,6 +15,9 @@ import { formSteps } from "@/application/routing/formSteps";
 import { PlanMitBeliebigenElternteilen } from "@/monatsplaner";
 
 export function BeispielePage() {
+  // TODO: Implement keyboard navigation and test accessibility tree
+  // TODO: Implement active option after navigating back from planer
+
   const store = useAppStore();
   const navigate = useNavigate();
 
@@ -22,12 +26,17 @@ export function BeispielePage() {
   const navigateToEinkommenPage = async () => {
     await navigate(formSteps.einkommen.route);
   };
-  const navigateToRechnerUndPlanerPage = async () => {
-    await navigateWithPlanState(formSteps.rechnerUndPlaner.route, plan);
-  };
 
   const ausgangslage = composeAusgangslageFuerPlaner(store.getState());
   const [plan, setPlan] = useState<PlanMitBeliebigenElternteilen>();
+
+  // TODO: I tried to type state as BeispielIdentifier | "KeineAuswahl" | "EigenePlanung"
+  // but all are strings, so typescript considers the literals redundant and the types clash.
+
+  const KeineAuswahl = "Keine Auswahl";
+  const EigenePlanung = "Eigene Planung";
+
+  const [aktivesBeispiel, setAktivesBeispiel] = useState<string>(KeineAuswahl);
 
   const { beispiele } = useBeispieleService(ausgangslage, setPlan, {
     onWaehleBeispielAus: trackMetricsForEinBeispielWurdeAusgewaehlt,
@@ -35,27 +44,64 @@ export function BeispielePage() {
 
   const { berechneElterngeldbezuegeByPlan } = useBerechneElterngeldbezuege();
 
+  const handleBeispielChange = (aktivierteOption: string) => {
+    const neuesAktivesBeispiel = beispiele.find(
+      (beispiel) => beispiel.identifier === aktivierteOption,
+    );
+
+    if (neuesAktivesBeispiel) {
+      setPlan(neuesAktivesBeispiel.plan);
+
+      trackMetricsForEinBeispielWurdeAusgewaehlt({
+        identifier: neuesAktivesBeispiel.identifier,
+      });
+    } else if (aktivierteOption === EigenePlanung) {
+      setPlan({
+        ausgangslage: composeAusgangslageFuerPlaner(store.getState()),
+        lebensmonate: {},
+      });
+    }
+
+    setAktivesBeispiel(aktivierteOption);
+  };
+
+  const navigateToRechnerUndPlanerPage = async () => {
+    await navigateWithPlanState(formSteps.rechnerUndPlaner.route, plan);
+  };
+
   return (
     <Page step={formSteps.beispiele}>
       <div className="flex flex-col gap-56">
         <BeispielAuswahloptionLegende beispiele={beispiele} />
 
-        <div className="grid grid-cols-1 gap-26 md:grid-cols-2">
+        <div
+          className="grid grid-cols-1 gap-26 md:grid-cols-2"
+          role="radiogroup"
+          aria-label="Beispielauswahl"
+        >
           {beispiele.map((beispiel) => (
             <BeispielAuswahlbox
+              titel={beispiel.titel}
               key={beispiel.identifier}
-              berechneElterngeldbezuege={berechneElterngeldbezuegeByPlan}
-              beispiel={beispiel}
-            />
+              beschreibung={beispiel.beschreibung}
+              checked={aktivesBeispiel === beispiel.identifier}
+              onChange={() => handleBeispielChange(beispiel.identifier)}
+            >
+              <BeispielAuswahlboxBody
+                beispiel={beispiel}
+                berechneElterngeldbezuege={berechneElterngeldbezuegeByPlan}
+              />
+            </BeispielAuswahlbox>
           ))}
 
-          <div className="flex flex-col rounded bg-off-white p-24 md:col-span-2">
-            <h4>Eigene Planung anlegen</h4>
-            <p>
-              Sie probieren selbst aus, wie Sie Ihr Elterngeld aufteilen und
-              erstellen Sie eine Planung ohne Planungshilfe.
-            </p>
-          </div>
+          <BeispielAuswahlbox
+            titel="Eigene Planung anlegen"
+            beschreibung="Sie probieren selbst aus, wie Sie Ihr Elterngeld aufteilen und
+            erstellen Sie eine Planung ohne Planungshilfe."
+            checked={aktivesBeispiel === EigenePlanung}
+            onChange={() => handleBeispielChange(EigenePlanung)}
+            className="md:col-span-2"
+          />
         </div>
 
         <div className="flex gap-16">

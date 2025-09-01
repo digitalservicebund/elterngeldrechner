ae5bb5701686069892336736cb623164b16b3161
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2025-09-01 13:27:14 +0200

    feat(planungshilfen): increase css specificity

--- src/application/features/beispiele/component/Auswahl/Radiobutton.tsx
+++ src/application/features/beispiele/component/Auswahl/Radiobutton.tsx
@@ -59,7 +59,7 @@ export function Radiobutton({
         <div className="col-span-2 mt-16 h-[1px] w-full bg-grey" />
       )}
 
-      <div className="col-span-2 flex flex-col gap-4 peer-checked:[&_span]:bg-grey-light">
+      <div className="col-span-2 flex flex-col gap-4 peer-checked:[&_.bg-primary-light]:bg-grey-light">
         {children}
       </div>
     </label>

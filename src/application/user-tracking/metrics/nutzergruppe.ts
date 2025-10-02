7c66f721fbd903af5168fd135e7375db2e5535a5
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2025-10-02 15:12:31 +0200

    fix: nutzergruppe before pregnancy

--- src/application/user-tracking/metrics/nutzergruppe.ts
+++ src/application/user-tracking/metrics/nutzergruppe.ts
@@ -10,7 +10,9 @@ export function trackNutzergruppe(birthdate: Date): void {
 function determineNutzergruppe(today: Date, birthdate: Date) {
   const startOfPregnancy = subWeeks(birthdate, 40);
 
-  if (isBefore(today, addWeeks(startOfPregnancy, 13))) {
+  if (isBefore(today, startOfPregnancy)) {
+    return "Kinderwunsch vor Schwangerschaft";
+  } else if (isBefore(today, addWeeks(startOfPregnancy, 13))) {
     return "werdende Eltern (1. Trimester)";
   } else if (isBefore(today, addWeeks(startOfPregnancy, 27))) {
     return "werdende Eltern (2. Trimester)";
@@ -91,12 +93,21 @@ if (import.meta.vitest) {
     });
 
     it("returns 'werdende Eltern (1. Trimester)' on the last menstrual period", () => {
-      const birthdate = new Date("2024-10-07");
-      const today = new Date("2024-01-01");
+      const birthdate = new Date("2025-01-01");
+      const today = new Date("2024-03-27");
 
       const nutzergruppe = determineNutzergruppe(today, birthdate);
 
       expect(nutzergruppe).toEqual("werdende Eltern (1. Trimester)");
+    });
+
+    it("returns 'Kinderwunsch vor Schwangerschaft' before the pregnancy", () => {
+      const birthdate = new Date("2025-01-01");
+      const today = new Date("2024-03-26");
+
+      const nutzergruppe = determineNutzergruppe(today, birthdate);
+
+      expect(nutzergruppe).toEqual("Kinderwunsch vor Schwangerschaft");
     });
   });
 }

22eb222d107a89707e578e90a2350791444bd4e0
Jakob Kempendorf <jakob.kempendorf@digitalservice.bund.de>
2025-11-07 16:41:36 +0100

    feat: add package mutterschutzrechner
    This commit adds the package mutterschutzrechner,
    which calculates the start and end date of the
    mutterschutz by entering only the expected date
    of birth of both the expected date and the
    actual date and, if applicable, also a reason
    for a longer time period after the birth.
    It includes all the necessary calculations, types
    and tests for this package.

---
+++ src/mutterschutzrechner/constants.ts
@@ -0,0 +1,14 @@
+/** Tag in Millisekunden */
+export const TAGE_IN_MS = 1000 * 60 * 60 * 24;
+
+/** 6 Wochen vor der Geburt (in Tagen) */
+export const MUTTERSCHUTZ_TAGE_VOR_GEBURT_STANDARD = 42;
+
+/** 8 Wochen nach der Geburt (in Tagen) */
+export const MUTTERSCHUTZ_TAGE_NACH_GEBURT_STANDARD = 56;
+
+/** 12 Wochen nach der Geburt bei Frühgeburt, Mehrlingen, Behinderung (in Tagen) */
+export const MUTTERSCHUTZ_TAGE_NACH_GEBURT_VERLAENGERT = 84;
+
+/** 14 Wochen (98 Tage) Gesamtdauer ohne Verlängerung */
+export const MUTTERSCHUTZ_DAUER_GESAMT_STANDARD = 98;

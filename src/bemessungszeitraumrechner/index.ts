b2e431d3d3e7dbd554031cbe179f9bb44196f9b8
Jakob Kempendorf <jakob.kempendorf@digitalservice.bund.de>
2025-11-12 17:06:08 +0100

    feat: add exact calculations with Ausklammerungen to bmz-rechner package
    This commit adds all the necessary calculations
    to get the exact Bemessungszeitraum, if there are
    certain periods of Ausklammerung, which should be
    excluded.
    It also changes the type Einklammerung by making
    monate not optional and removing beschreibung.
    This also made additions to the standard
    calculations necessary.

--- src/bemessungszeitraumrechner/index.ts
+++ src/bemessungszeitraumrechner/index.ts
@@ -2,6 +2,7 @@ export {
   berechneRelevanteBemessungszeitraeumeFuerErsteEinordnung,
   berechneUngefaehrenBemessungszeitraum,
 } from "./berechneUngefaehreBemessungszeitraeume";
+export { berechneGenauenBemessungszeitraum } from "./berechneGenaueBemessungszeitraeume";
 export type { Ausklammerung } from "./Ausklammerung";
 export type { Bemessungszeitraum } from "./Bemessungszeitraum";
 export type { Erwerbstaetigkeit } from "./Erwerbstaetigkeit";

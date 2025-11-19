6ae0cb3dba1f99ef6eeb69d7226f25d700976839
Jakob Kempendorf <jakob.kempendorf@digitalservice.bund.de>
2025-11-19 13:37:25 +0100

    feat: changes to mutterschutzrechner logic and tests
    This commit changes the logic in which the
    startdatum is calculated by setting the geburtsdatum
    as the startdatum if the birth was more than six
    weeks prior to the expected date.
    It also changes the calculation of the
    additional period after the birth by adding all
    days for which the actual birth took place before the
    expected birth.
    Furthermore it refactors the tests to use test.each with
    defined test cases.
    And finally there are some small changes:
    - add relevant paragraphs of the law to the docs
    - add branded type for Mutterschutz
    - make constants self-documenting
    - add one constant and rename one

--- src/mutterschutzrechner/constants.ts
+++ src/mutterschutzrechner/constants.ts
@@ -1,14 +1,6 @@
-/** Tag in Millisekunden */
-export const TAGE_IN_MS = 1000 * 60 * 60 * 24;
-
-/** 6 Wochen vor der Geburt (in Tagen) */
-export const MUTTERSCHUTZ_TAGE_VOR_GEBURT_STANDARD = 42;
-
-/** 8 Wochen nach der Geburt (in Tagen) */
-export const MUTTERSCHUTZ_TAGE_NACH_GEBURT_STANDARD = 56;
-
-/** 12 Wochen nach der Geburt bei Frühgeburt, Mehrlingen, Behinderung (in Tagen) */
-export const MUTTERSCHUTZ_TAGE_NACH_GEBURT_VERLAENGERT = 84;
-
-/** 14 Wochen (98 Tage) Gesamtdauer ohne Verlängerung */
-export const MUTTERSCHUTZ_DAUER_GESAMT_STANDARD = 98;
+export const TAG_IN_MS = 1000 * 60 * 60 * 24;
+export const MUTTERSCHUTZ_TAGE_VOR_GEBURT_STANDARD = 6 * 7;
+export const MUTTERSCHUTZ_TAGE_NACH_GEBURT_STANDARD = 8 * 7;
+export const MUTTERSCHUTZ_TAGE_NACH_GEBURT_VERLAENGERT = 12 * 7;
+export const MUTTERSCHUTZ_DAUER_GESAMT_STANDARD = 14 * 7;
+export const MUTTERSCHUTZ_DAUER_GESAMT_VERLAENGERT = 18 * 7;

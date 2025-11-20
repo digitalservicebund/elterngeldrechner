6c50fc314c5ed52354e8812432848bcf0a48994c
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2025-11-20 11:56:14 +0100

    refactor: mutterschutzrechner in source tests

--- src/mutterschutzrechner/berechneMutteschutz.ts
+++ src/mutterschutzrechner/berechneMutteschutz.ts
@@ -105,99 +105,108 @@ const weeks = (value: number) => value * 7 * 24 * 60 * 60 * 1000;
 if (import.meta.vitest) {
   const { describe, expect, test } = import.meta.vitest;
 
-  const createDate = (datumsString: string) => new Date(datumsString);
-
   describe("berechneMutterschutz", () => {
-    test.each(mutterschutzTestfaelle)(
-      "$testfall",
-      ({
+    test.each(mutterschutzTestfaelle)("$beschreibung", (testfall: Testfall) => {
+      const {
         errechneterGeburtstermin,
         tatsaechlichesGeburtsdatum,
         hatVerlaengerungsGrund,
         erwartetesStartdatum,
         erwartetesEnddatum,
-      }) => {
-        const { startdatum, enddatum } = berechneMutterschutz(
-          createDate(errechneterGeburtstermin),
-          tatsaechlichesGeburtsdatum
-            ? createDate(tatsaechlichesGeburtsdatum)
-            : undefined,
-          hatVerlaengerungsGrund,
-        );
+      } = testfall;
 
-        expect(startdatum).toEqual(createDate(erwartetesStartdatum));
-        expect(enddatum).toEqual(createDate(erwartetesEnddatum));
-      },
-    );
+      const { startdatum, enddatum } = berechneMutterschutz(
+        createDate(errechneterGeburtstermin),
+        createDate(tatsaechlichesGeburtsdatum),
+        hatVerlaengerungsGrund,
+      );
+
+      expect(startdatum).toEqual(createDate(erwartetesStartdatum));
+      expect(enddatum).toEqual(createDate(erwartetesEnddatum));
+    });
   });
-}
 
-interface MutterschutzTestfall {
-  testfall: string;
-  errechneterGeburtstermin: string;
-  tatsaechlichesGeburtsdatum?: string;
-  hatVerlaengerungsGrund?: boolean;
-  erwartetesStartdatum: string;
-  erwartetesEnddatum: string;
-}
+  const mutterschutzTestfaelle: Testfall[] = [
+    {
+      beschreibung:
+        "Standard nach § 3 MuSchG (Ungeboren) - 6 Wochen vor ET / 8 Wochen nach ET",
+      errechneterGeburtstermin: "2025-10-15T00:00:00.000Z",
+      erwartetesStartdatum: "2025-09-03T00:00:00.000Z",
+      erwartetesEnddatum: "2025-12-10T00:00:00.000Z",
+    },
+    {
+      beschreibung:
+        "Standard nach § 3 Abs. 2 Satz 1 (Geburt nach ET) - Schutzfrist wird nach hinten verschoben",
+      errechneterGeburtstermin: "2025-10-15T00:00:00.000Z",
+      tatsaechlichesGeburtsdatum: "2025-10-16T00:00:00.000Z",
+      erwartetesStartdatum: "2025-09-03T00:00:00.000Z",
+      erwartetesEnddatum: "2025-12-11T00:00:00.000Z",
+    },
+    {
+      beschreibung:
+        "Standard nach § 3 Abs. 2 Satz 3 (Geburt vor ET) - Gesamtdauer von 14 Wochen gilt",
+      errechneterGeburtstermin: "2025-10-15T00:00:00.000Z",
+      tatsaechlichesGeburtsdatum: "2025-10-14T00:00:00.000Z",
+      erwartetesStartdatum: "2025-09-03T00:00:00.000Z",
+      erwartetesEnddatum: "2025-12-10T00:00:00.000Z",
+    },
+    {
+      beschreibung:
+        "Z.B. Frühgeburt nach § 3 Abs. 2 Satz 2 Nummer 1 MuSchG (Geburt mehr als 6 Wochen vor ET) - Startdatum entspricht Geburtsdatum und Gesamtdauer von 18 Wochen gilt",
+      errechneterGeburtstermin: "2025-10-15T00:00:00.000Z",
+      tatsaechlichesGeburtsdatum: "2025-09-02T00:00:00.000Z",
+      hatVerlaengerungsGrund: true,
+      erwartetesStartdatum: "2025-09-02T00:00:00.000Z",
+      erwartetesEnddatum: "2026-01-06T00:00:00.000Z",
+    },
+    {
+      beschreibung:
+        "Z.B. Mehrlinge nach § 3 Abs. 2 Satz 2 Nummer 2 MuSchG (ungeboren) - Verlängerte Schutzfrist von 12 Wochen nach ET",
+      errechneterGeburtstermin: "2025-10-15T00:00:00.000Z",
+      hatVerlaengerungsGrund: true,
+      erwartetesStartdatum: "2025-09-03T00:00:00.000Z",
+      erwartetesEnddatum: "2026-01-07T00:00:00.000Z",
+    },
+    {
+      beschreibung:
+        "Z.B. Mehrlinge nach § 3 Abs. 2 Satz 2 Nummer 2 MuSchG (Geburt vor ET) - Gesamtdauer von 18 Wochen gilt",
+      errechneterGeburtstermin: "2025-10-15T00:00:00.000Z",
+      tatsaechlichesGeburtsdatum: "2025-10-14T00:00:00.000Z",
+      hatVerlaengerungsGrund: true,
+      erwartetesStartdatum: "2025-09-03T00:00:00.000Z",
+      erwartetesEnddatum: "2026-01-07T00:00:00.000Z",
+    },
+    {
+      beschreibung:
+        "Z.B. Behinderung nach § 3 Abs. 2 Satz 2 Nummer 3 MuSchG (Geburt nach ET) - Verlängerte Schutzfrist wird nach hinten verschoben",
+      errechneterGeburtstermin: "2025-10-15T00:00:00.000Z",
+      tatsaechlichesGeburtsdatum: "2025-10-16T00:00:00.000Z",
+      hatVerlaengerungsGrund: true,
+      erwartetesStartdatum: "2025-09-03T00:00:00.000Z",
+      erwartetesEnddatum: "2026-01-08T00:00:00.000Z",
+    },
+  ];
 
-const mutterschutzTestfaelle: MutterschutzTestfall[] = [
-  {
-    testfall:
-      "Standard nach § 3 MuSchG (Ungeboren) - 6 Wochen vor ET / 8 Wochen nach ET",
-    errechneterGeburtstermin: "2025-10-15T00:00:00.000Z",
-    erwartetesStartdatum: "2025-09-03T00:00:00.000Z",
-    erwartetesEnddatum: "2025-12-10T00:00:00.000Z",
-  },
-  {
-    testfall:
-      "Standard nach § 3 Abs. 2 Satz 1 (Geburt nach ET) - Schutzfrist wird nach hinten verschoben",
-    errechneterGeburtstermin: "2025-10-15T00:00:00.000Z",
-    tatsaechlichesGeburtsdatum: "2025-10-16T00:00:00.000Z",
-    erwartetesStartdatum: "2025-09-03T00:00:00.000Z",
-    erwartetesEnddatum: "2025-12-11T00:00:00.000Z",
-  },
-  {
-    testfall:
-      "Standard nach § 3 Abs. 2 Satz 3 (Geburt vor ET) - Gesamtdauer von 14 Wochen gilt",
-    errechneterGeburtstermin: "2025-10-15T00:00:00.000Z",
-    tatsaechlichesGeburtsdatum: "2025-10-14T00:00:00.000Z",
-    erwartetesStartdatum: "2025-09-03T00:00:00.000Z",
-    erwartetesEnddatum: "2025-12-10T00:00:00.000Z",
-  },
-  {
-    testfall:
-      "Z.B. Frühgeburt nach § 3 Abs. 2 Satz 2 Nummer 1 MuSchG (Geburt mehr als 6 Wochen vor ET) - Startdatum entspricht Geburtsdatum und Gesamtdauer von 18 Wochen gilt",
-    errechneterGeburtstermin: "2025-10-15T00:00:00.000Z",
-    tatsaechlichesGeburtsdatum: "2025-09-02T00:00:00.000Z",
-    hatVerlaengerungsGrund: true,
-    erwartetesStartdatum: "2025-09-02T00:00:00.000Z",
-    erwartetesEnddatum: "2026-01-06T00:00:00.000Z",
-  },
-  {
-    testfall:
-      "Z.B. Mehrlinge nach § 3 Abs. 2 Satz 2 Nummer 2 MuSchG (ungeboren) - Verlängerte Schutzfrist von 12 Wochen nach ET",
-    errechneterGeburtstermin: "2025-10-15T00:00:00.000Z",
-    hatVerlaengerungsGrund: true,
-    erwartetesStartdatum: "2025-09-03T00:00:00.000Z",
-    erwartetesEnddatum: "2026-01-07T00:00:00.000Z",
-  },
-  {
-    testfall:
-      "Z.B. Mehrlinge nach § 3 Abs. 2 Satz 2 Nummer 2 MuSchG (Geburt vor ET) - Gesamtdauer von 18 Wochen gilt",
-    errechneterGeburtstermin: "2025-10-15T00:00:00.000Z",
-    tatsaechlichesGeburtsdatum: "2025-10-14T00:00:00.000Z",
-    hatVerlaengerungsGrund: true,
-    erwartetesStartdatum: "2025-09-03T00:00:00.000Z",
-    erwartetesEnddatum: "2026-01-07T00:00:00.000Z",
-  },
-  {
-    testfall:
-      "Z.B. Behinderung nach § 3 Abs. 2 Satz 2 Nummer 3 MuSchG (Geburt nach ET) - Verlängerte Schutzfrist wird nach hinten verschoben",
-    errechneterGeburtstermin: "2025-10-15T00:00:00.000Z",
-    tatsaechlichesGeburtsdatum: "2025-10-16T00:00:00.000Z",
-    hatVerlaengerungsGrund: true,
-    erwartetesStartdatum: "2025-09-03T00:00:00.000Z",
-    erwartetesEnddatum: "2026-01-08T00:00:00.000Z",
-  },
-];
+  type Testfall = {
+    beschreibung: string;
+    errechneterGeburtstermin: string;
+    tatsaechlichesGeburtsdatum?: string;
+    hatVerlaengerungsGrund?: boolean;
+    erwartetesStartdatum: string;
+    erwartetesEnddatum: string;
+  };
+
+  type NonUndefined<T> = T extends undefined ? never : T;
+
+  const createDate = <T extends string | undefined>(
+    datumsString: T,
+  ): T extends undefined ? undefined : Date => {
+    if (datumsString) {
+      return new Date(datumsString as NonUndefined<T>) as T extends undefined
+        ? undefined
+        : Date;
+    } else {
+      return undefined as T extends undefined ? undefined : Date;
+    }
+  };
+}

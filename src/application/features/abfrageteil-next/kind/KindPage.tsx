f8116404fc73807811ef2b67aa34cb50054451b0
Jakob Kempendorf <jakob.kempendorf@digitalservice.bund.de>
2026-02-06 12:03:57 +0100

    feat(abfrageteil): apply design and logic to KindPage

--- src/application/features/abfrageteil-next/kind/KindPage.tsx
+++ src/application/features/abfrageteil-next/kind/KindPage.tsx
@@ -2,45 +2,82 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useId } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router";
-import { GeburtSchema } from "./KindSchema";
+import { Geburt, GeburtSchema } from "./KindSchema";
 import { Button, CustomRadioGroup } from "@/application/components";
+import { Page } from "@/application/features/abfrageteil-next/components/Page";
+import { useEventContext } from "@/application/features/abfrageteil-next/events/EventContext";
+import {
+  Route,
+  getNextRoute,
+} from "@/application/features/abfrageteil-next/routing/routing";
+import { encodeSafely } from "@/application/features/abfrageteil-next/zod";
 
 export function KindPage() {
+  const { dispatch, findLastEvent } = useEventContext();
+
   const formIdentifier = useId();
   const navigate = useNavigate();
 
+  const currentRoute = Route.KindAbfrage;
+  const lastEvent = findLastEvent(currentRoute);
+
   const { register, handleSubmit, formState } = useForm({
     resolver: zodResolver(GeburtSchema),
+    defaultValues: encodeSafely(GeburtSchema, lastEvent),
   });
-  const { errors } = formState;
 
-  const navigateNextPage = () => {
-    void navigate("/abfrageteil-next/kind/geboren");
+  const { errors: formErrors } = formState;
+
+  const onSubmit = (values: Geburt) => {
+    dispatch({
+      route: currentRoute,
+      payload: values,
+    });
+
+    const nextPath = getNextRoute({
+      route: currentRoute,
+      payload: values,
+    });
+
+    void navigate(`/abfrageteil-next${nextPath}`);
   };
 
   return (
-    <>
-      <h2>Angaben zum Kind</h2>
+    <Page
+      heading="Allgemeine zum Kind"
+      navigationItems={[]}
+      currentNavigationItem=""
+    >
       <form
         id={formIdentifier}
-        className="flex flex-col gap-56"
-        onSubmit={handleSubmit(navigateNextPage)}
+        className="mt-40 flex flex-col gap-56"
+        onSubmit={handleSubmit(onSubmit)}
       >
-        <CustomRadioGroup
-          legend="Ist Ihr Kind schon geboren?"
-          errors={errors}
-          register={register}
-          name="istGeboren"
-          options={[
-            { value: "yes", label: "Ja" },
-            { value: "no", label: "Nein" },
-          ]}
-        />
+        <div>
+          <h3 className="mb-10">Ist ihr Kind schon geboren?</h3>
 
-        <Button type="submit" buttonStyle="primary" form={formIdentifier}>
-          Weiter
-        </Button>
+          <CustomRadioGroup
+            legend=""
+            errors={formErrors}
+            register={register}
+            name="istGeboren"
+            options={[
+              { value: "yes", label: "Ja" },
+              { value: "no", label: "Nein" },
+            ]}
+          />
+        </div>
+
+        <div className="flex gap-16">
+          <Button type="button" buttonStyle="secondary" onClick={() => {}}>
+            Zurück
+          </Button>
+
+          <Button type="submit" form={formIdentifier}>
+            Weiter
+          </Button>
+        </div>
       </form>
-    </>
+    </Page>
   );
 }

41f871d3f46d0759db93e1a0cc5e28caca2117a8
Jakob Kempendorf <jakob.kempendorf@digitalservice.bund.de>
2026-02-13 17:13:41 +0100

    ci: change env type

--- src/application/type-declarations/vite-env.d.ts
+++ src/application/type-declarations/vite-env.d.ts
@@ -3,8 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_APP_USER_TRACKING_TAG_MANAGER_SOURCE: string;
   readonly VITE_APP_PRELOAD_STATE: string;
-  readonly VITE_FEATURE_FLAG_ABFRAGETEIL_V2: boolean;
-  readonly VITE_FEATURE_FLAG_FAMILIENPORTAL_ASSET_STORAGE: boolean;
+  readonly VITE_FEATURE_FLAG_ABFRAGETEIL_V2: string;
+  readonly VITE_FEATURE_FLAG_FAMILIENPORTAL_ASSET_STORAGE: string;
 }
 
 interface ImportMeta {

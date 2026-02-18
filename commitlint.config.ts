39a05e2dfeb9443a8f1f9c116a5846d412f92445
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2026-02-18 16:46:10 +0100

    chore: drop body-max-line-length commitlint rule
    The body-max-line-length is becoming tedious in the every
    day work and doesn't work for long commit attributions which
    ultimately led to the decision to disable the rule

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -6,6 +6,5 @@ export default {
   rules: {
     "body-case": [RuleConfigSeverity.Error, "always", "sentence-case"],
     "header-max-length": [RuleConfigSeverity.Error, "always", 72],
-    "body-max-line-length": [RuleConfigSeverity.Error, "always", 72],
   },
 } satisfies UserConfig;

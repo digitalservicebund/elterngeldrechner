f78fbaafbd3ff1f35a8ee3452fb88adae48b6a63
Jakob Kempendorf <jakob.kempendorf@digitalservice.bund.de>
2026-02-19 14:48:55 +0100

    feat(abfrageteil): add inputs to ElternteilAusklammerungsGruendePage

--- src/application/features/abfrageteil-next/elternteil/ElternteilAusklammerungGruendePage.tsx
+++ src/application/features/abfrageteil-next/elternteil/ElternteilAusklammerungGruendePage.tsx
@@ -6,7 +6,9 @@ import {
   ElternteilAusklammerungGruende,
   ElternteilAusklammerungGruendeSchema,
 } from "./ElternteilSchema";
-import { Button } from "@/application/components";
+import { Button, InfoText } from "@/application/components";
+import { Alert } from "@/application/components/Alert";
+import { CustomCheckbox } from "@/application/features/abfrageteil/components/common";
 import { Page } from "@/application/features/abfrageteil-next/components/Page";
 import { useEventContext } from "@/application/features/abfrageteil-next/events/EventContext";
 import { useElternteilIndex } from "@/application/features/abfrageteil-next/hooks/usePageIndex";
@@ -30,8 +32,7 @@ export function ElternteilAusklammerungGruendePage() {
     elternteilIndex,
   });
 
-  // const { register, handleSubmit, formState } = useForm({
-  const { handleSubmit } = useForm({
+  const { register, handleSubmit, formState } = useForm({
     resolver: zodResolver(ElternteilAusklammerungGruendeSchema),
     defaultValues: encodeSafely(
       ElternteilAusklammerungGruendeSchema,
@@ -39,7 +40,7 @@ export function ElternteilAusklammerungGruendePage() {
     ),
   });
 
-  // const { errors: formErrors } = formState;
+  const { errors: formErrors } = formState;
 
   const onSubmit = (values: ElternteilAusklammerungGruende) => {
     const event: FormEvent = {
@@ -58,17 +59,58 @@ export function ElternteilAusklammerungGruendePage() {
   };
 
   return (
-    <Page heading="">
+    <Page heading="Finanzielle Situation">
       <form
         id={formIdentifier}
         className="mt-40 flex flex-col gap-56"
         onSubmit={handleSubmit(onSubmit)}
       >
         <div>
-          {/* <h3 className="mb-10"></h3> */}
+          <h3>Treffen folgende Gründe auf Sie zu?</h3>
 
-          {/* input */}
+          <InfoText
+            question="Warum fragen wir das?"
+            answer="Wenn Sie hier etwas auswählen, kann für die Berechnung Ihres Elterngeldes ein anderer Monat genommen werden. Und zwar der Monat, in dem Sie mehr verdient haben. "
+          />
+
+          <CustomCheckbox
+            className="mt-20"
+            register={register}
+            name="hatMutterschutzAelteresKind"
+            label="Ich war für ein älteres Kind im Mutterschutz"
+            errors={formErrors}
+          />
+
+          <CustomCheckbox
+            className="mt-20"
+            register={register}
+            name="hatElterngeldAelteresKind"
+            label="Ich habe für ein älteres Kind Elterngeld bekommen"
+            errors={formErrors}
+          />
+
+          <CustomCheckbox
+            className="mt-20"
+            register={register}
+            name="hatSchwangerschaftsbedingteErkrankung"
+            label="Ich hatte eine Erkrankung wegen meiner Schwangerschaft und hatte weniger Einkommen"
+            errors={formErrors}
+          />
+
+          <CustomCheckbox
+            className="mt-20"
+            register={register}
+            name="hatKeinAusklammerungsgruende"
+            label="Keiner der genannten Gründe"
+            errors={formErrors}
+          />
         </div>
+
+        <Alert headline="Gut zu wissen für die spätere Antragstellung:">
+          Wenn Sie vor der Geburt selbstständig waren und auf dieser Seite einen
+          Grund auswählen, können Sie später im Elterngeldantrag beantragen, den
+          Bemessungszeitraum aufgrund dieser Angaben um ein Jahr vorzuverlegen.
+        </Alert>
 
         <div className="flex gap-16">
           <Button type="button" buttonStyle="secondary" onClick={navigateBack}>

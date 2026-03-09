73af1251c6811e5f92ce343198b535a1f2c76856
Jakob Kempendorf <jakob.kempendorf@digitalservice.bund.de>
2026-03-09 17:57:37 +0100

    fix(abfrageteil): parametrized paths in route guard

--- src/application/features/abfrageteil-next/routing/RouteGuardAbfrageteil.tsx
+++ src/application/features/abfrageteil-next/routing/RouteGuardAbfrageteil.tsx
@@ -1,11 +1,15 @@
-import { Navigate, Outlet, useLocation } from "react-router";
+import { Navigate, Outlet, generatePath, useLocation } from "react-router";
+import { FormEvent } from "./FormEvent";
 import { Route } from "./Route";
 import { findeNaechstenPfad } from "./findeNaechstenPfad";
-import { generateAbfrageteilPath } from "./generatePath";
+import {
+  generateAbfrageteilPath,
+  generateParametrizedPath,
+} from "./generatePath";
 import { useEventContext } from "@/application/features/abfrageteil-next/events/EventContext";
 
 export function RouteGuardAbfrageteil() {
-  const { filtereValideEventHistorie } = useEventContext();
+  const { filtereValideEventHistorie, findeLetztesEvent } = useEventContext();
   const { pathname } = useLocation();
 
   const startseitePfad = generateAbfrageteilPath(Route.Startseite);
@@ -18,23 +22,174 @@ export function RouteGuardAbfrageteil() {
   }
 
   const valideEventHistorie = filtereValideEventHistorie();
-  const letztesValidesEvent =
-    valideEventHistorie[valideEventHistorie.length - 1];
+  const letztesValidesEvent = findeLetztesEvent();
 
-  const maximalErlaubterPfad = letztesValidesEvent
-    ? findeNaechstenPfad(letztesValidesEvent)
-    : allgemeineAngabenPfad;
+  if (
+    valideEventHistorie[0]?.route === undefined ||
+    letztesValidesEvent === undefined
+  ) {
+    return <Navigate to={allgemeineAngabenPfad} replace />;
+  }
 
-  const istAufAbgeschlossenerSeite = valideEventHistorie.some(
-    (event) => generateAbfrageteilPath(event.route) === pathname,
+  const letzteErlaubteRoute = findeLetzteErlaubteRoute(
+    valideEventHistorie,
+    letztesValidesEvent,
+    pathname,
   );
-  const istAufNaechsterErlaubterSeite = pathname === maximalErlaubterPfad;
-  const pfadIstNochNichtErlaubt =
-    !istAufAbgeschlossenerSeite && !istAufNaechsterErlaubterSeite;
 
-  if (pfadIstNochNichtErlaubt) {
-    return <Navigate to={maximalErlaubterPfad} replace />;
+  if (letzteErlaubteRoute) {
+    return <Navigate to={letzteErlaubteRoute} replace />;
   }
 
   return <Outlet />;
+}
+
+function findeLetzteErlaubteRoute(
+  valideEventHistorie: FormEvent[],
+  letztesValidesEvent: FormEvent,
+  aktuelleRoute: string,
+) {
+  const maximalErlaubterPfad = findeNaechstenPfad(letztesValidesEvent);
+
+  if (aktuelleRoute === maximalErlaubterPfad) {
+    return undefined;
+  }
+
+  const istAufAbgeschlossenerSeite = valideEventHistorie.some(
+    (event) => erstellePfadAusEvent(event) === aktuelleRoute,
+  );
+
+  return istAufAbgeschlossenerSeite ? undefined : maximalErlaubterPfad;
+}
+
+const erstellePfadAusEvent = (event: FormEvent): string => {
+  if ("params" in event) {
+    // @ts-expect-error: Die Korrelation zwischen route und params ist durch die FormEvent Union sichergestellt
+    const path = generateParametrizedPath(event.route, event.params);
+    return generateAbfrageteilPath(path);
+  }
+  return generateAbfrageteilPath(generatePath(event.route));
+};
+
+if (import.meta.vitest) {
+  const { describe, it, expect } = import.meta.vitest;
+
+  describe("findeLetzteErlaubteRoute", () => {
+    it("it returns undefined when aktuelleRoute is next possible page", () => {
+      const eventStream: FormEvent[] = [
+        {
+          route: Route.ElternteilAllgemeineAngaben,
+          params: { elternteilIndex: 0 },
+          payload: {
+            name: "Hanna",
+            istAlleinerziehend: false,
+            istImMutterschutz: true,
+          },
+        },
+      ];
+      const letztesEvent: FormEvent = {
+        route: Route.ElternteilAllgemeineAngaben,
+        params: { elternteilIndex: 0 },
+        payload: {
+          name: "Hanna",
+          istAlleinerziehend: false,
+          istImMutterschutz: true,
+        },
+      };
+
+      const result = findeLetzteErlaubteRoute(
+        eventStream,
+        letztesEvent,
+        "/abfrageteil/elternteil/0/finanzielles/ausklammerung/gruende",
+      );
+
+      expect(result).toBeUndefined();
+    });
+
+    it("it returns undefined when aktuelleRoute is in valideEventHistorie", () => {
+      const eventStream: FormEvent[] = [
+        {
+          route: Route.ElternteilAllgemeineAngaben,
+          params: { elternteilIndex: 0 },
+          payload: {
+            name: "Hanna",
+            istAlleinerziehend: false,
+            istImMutterschutz: true,
+          },
+        },
+        {
+          route: Route.ElternteilAusklammerungGruendeAngaben,
+          params: { elternteilIndex: 0 },
+          payload: {
+            hatKeineAusklammerungsgruende: false,
+            hatMutterschutzAelteresKind: true,
+            hatElterngeldAelteresKind: false,
+            hatSchwangerschaftsbedingteErkrankung: false,
+          },
+        },
+      ];
+      const letztesEvent: FormEvent = {
+        route: Route.ElternteilAusklammerungGruendeAngaben,
+        params: { elternteilIndex: 0 },
+        payload: {
+          hatKeineAusklammerungsgruende: false,
+          hatMutterschutzAelteresKind: true,
+          hatElterngeldAelteresKind: false,
+          hatSchwangerschaftsbedingteErkrankung: false,
+        },
+      };
+
+      const result = findeLetzteErlaubteRoute(
+        eventStream,
+        letztesEvent,
+        "/abfrageteil/elternteil/0",
+      );
+
+      expect(result).toBeUndefined();
+    });
+
+    it("it returns '/abfrageteil/elternteil/0/finanzielles/ausklammerung/zeiten' when aktuelleRoute is neither in valideEventHistorie nor next possible page", () => {
+      const eventStream: FormEvent[] = [
+        {
+          route: Route.ElternteilAllgemeineAngaben,
+          params: { elternteilIndex: 0 },
+          payload: {
+            name: "Hanna",
+            istAlleinerziehend: false,
+            istImMutterschutz: true,
+          },
+        },
+        {
+          route: Route.ElternteilAusklammerungGruendeAngaben,
+          params: { elternteilIndex: 0 },
+          payload: {
+            hatKeineAusklammerungsgruende: false,
+            hatMutterschutzAelteresKind: true,
+            hatElterngeldAelteresKind: false,
+            hatSchwangerschaftsbedingteErkrankung: false,
+          },
+        },
+      ];
+      const letztesEvent: FormEvent = {
+        route: Route.ElternteilAusklammerungGruendeAngaben,
+        params: { elternteilIndex: 0 },
+        payload: {
+          hatKeineAusklammerungsgruende: false,
+          hatMutterschutzAelteresKind: true,
+          hatElterngeldAelteresKind: false,
+          hatSchwangerschaftsbedingteErkrankung: false,
+        },
+      };
+
+      const result = findeLetzteErlaubteRoute(
+        eventStream,
+        letztesEvent,
+        "/abfrageteil/elternteil/0/finanzielles/taetigkeit/abfrage",
+      );
+
+      expect(result).toEqual(
+        "/abfrageteil/elternteil/0/finanzielles/ausklammerung/zeiten",
+      );
+    });
+  });
 }

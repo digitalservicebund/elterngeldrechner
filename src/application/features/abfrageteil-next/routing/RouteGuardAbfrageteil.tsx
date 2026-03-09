08c433d610e5fef81d58a161ebe38f2408451280
Jakob Kempendorf <jakob.kempendorf@digitalservice.bund.de>
2026-03-09 15:12:39 +0100

    feat(abfrageteil): add route guards for new abfrageteil

---
+++ src/application/features/abfrageteil-next/routing/RouteGuardAbfrageteil.tsx
@@ -0,0 +1,40 @@
+import { Navigate, Outlet, useLocation } from "react-router";
+import { Route } from "./Route";
+import { findeNaechstenPfad } from "./findeNaechstenPfad";
+import { generateAbfrageteilPath } from "./generatePath";
+import { useEventContext } from "@/application/features/abfrageteil-next/events/EventContext";
+
+export function RouteGuardAbfrageteil() {
+  const { filtereValideEventHistorie } = useEventContext();
+  const { pathname } = useLocation();
+
+  const startseitePfad = generateAbfrageteilPath(Route.Startseite);
+  const allgemeineAngabenPfad = generateAbfrageteilPath(
+    Route.AllgemeineAngaben,
+  );
+
+  if (pathname === startseitePfad || pathname === allgemeineAngabenPfad) {
+    return <Outlet />;
+  }
+
+  const valideEventHistorie = filtereValideEventHistorie();
+  const letztesValidesEvent =
+    valideEventHistorie[valideEventHistorie.length - 1];
+
+  const maximalErlaubterPfad = letztesValidesEvent
+    ? findeNaechstenPfad(letztesValidesEvent)
+    : allgemeineAngabenPfad;
+
+  const istAufAbgeschlossenerSeite = valideEventHistorie.some(
+    (event) => generateAbfrageteilPath(event.route) === pathname,
+  );
+  const istAufNaechsterErlaubterSeite = pathname === maximalErlaubterPfad;
+  const pfadIstNochNichtErlaubt =
+    !istAufAbgeschlossenerSeite && !istAufNaechsterErlaubterSeite;
+
+  if (pfadIstNochNichtErlaubt) {
+    return <Navigate to={maximalErlaubterPfad} replace />;
+  }
+
+  return <Outlet />;
+}

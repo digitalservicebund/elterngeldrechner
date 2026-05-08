30a95d46a08ef1b301ef3eb68297eb09709d3c0a
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2026-05-08 14:43:17 +0200

    feat(tracking): integrate posthog gated by a feature flag
    After starting to play around with the posthog client, orignally
    only to gather information for better estimates in our planning,
    I ended up with some useful code and added it under a feature
    flag to not throw it away.
    
    The configuration is kept as minimal as possible for nor avoiding
    accidental data collection. Subsequent commits enable the feature
    we actually want.
    
    Since posthog-js declares its license under a custom name it was
    added as an exception to the license checker but under the hood
    is just apache 2.0.

--- src/application/type-declarations/vite-env.d.ts
+++ src/application/type-declarations/vite-env.d.ts
@@ -2,9 +2,15 @@
 
 interface ImportMetaEnv {
   readonly VITE_APP_USER_TRACKING_TAG_MANAGER_SOURCE: string;
+
   readonly VITE_APP_PRELOAD_STATE: string;
+
+  readonly VITE_FEATURE_FLAG_POSTHOG: string;
   readonly VITE_FEATURE_FLAG_ABFRAGETEIL_V2: string;
   readonly VITE_FEATURE_FLAG_FAMILIENPORTAL_ASSET_STORAGE: string;
+
+  readonly VITE_PUBLIC_POSTHOG_PROJECT_TOKEN: string;
+  readonly VITE_PUBLIC_POSTHOG_HOST: string;
 }
 
 interface ImportMeta {

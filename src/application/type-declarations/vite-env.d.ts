ba5592b014c642da7650759488c6490ba816d35e
Dennis Dierkes <dennis.dierkes@digitalservice.bund.de>
2026-05-13 19:52:34 +0200

    refactor: remove stale variable from vite-env.d.ts

--- src/application/type-declarations/vite-env.d.ts
+++ src/application/type-declarations/vite-env.d.ts
@@ -7,8 +7,6 @@ interface ImportMetaEnv {
 
   readonly VITE_FEATURE_FLAG_POSTHOG: string;
   readonly VITE_FEATURE_FLAG_ABFRAGETEIL_V2: string;
-  readonly VITE_FEATURE_FLAG_FAMILIENPORTAL_ASSET_STORAGE: string;
-
   readonly VITE_PUBLIC_POSTHOG_PROJECT_TOKEN: string;
   readonly VITE_PUBLIC_POSTHOG_HOST: string;
 }

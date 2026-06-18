cda8a30677734e9a2389898ae0e9456f6c4259b1
Jakob Kempendorf <jakob.kempendorf@digitalservice.bund.de>
2026-06-18 15:45:06 +0200

    feat: remove skip button for ausklammerungen

--- src/application/features/abfrageteil/pages/elternteil/ElternteilAusklammerungMutterschutzAbfragePage.tsx
+++ src/application/features/abfrageteil/pages/elternteil/ElternteilAusklammerungMutterschutzAbfragePage.tsx
@@ -1,4 +1,3 @@
-import DoubleArrowIcon from "~icons/material-symbols/double-arrow";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useId } from "react";
 import { useForm } from "react-hook-form";
@@ -96,10 +95,6 @@ export function ElternteilAusklammerungMutterschutzAbfragePage() {
 
   const navigateBack = async () => {
     await navigate(findeVorherigenPfad(currentRoute, routeParams));
-  };
-
-  const handleSkip = async () => {
-    await onSubmit({ hatMutterschutzGeschwisterkind: undefined });
   };
 
   const vorname = findeVornamen(eventStream, routeParams.elternteilIndex);
@@ -210,17 +205,6 @@ export function ElternteilAusklammerungMutterschutzAbfragePage() {
           <Button type="submit" form={formIdentifier}>
             Weiter
           </Button>
-
-          <button
-            type="button"
-            className="ml-56 border-none bg-transparent p-0 !text-base text-primary active:focus:outline-none [@media(hover:hover)]:hover:bg-transparent"
-            onClick={handleSkip}
-          >
-            <div className="flex items-center">
-              <DoubleArrowIcon className="mr-4 mt-4" />
-              <span className="font-bold">Frage überspringen</span>
-            </div>
-          </button>
         </div>
       </form>
     </Page>
